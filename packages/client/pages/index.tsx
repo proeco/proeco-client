d20d382130b6d035abc9aa86d0c6149d551d1a9a
itizawa <itizawa.pen@gmail.com>
2021-09-23 21:19:29 +0900

    ✨ setup

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { User } from '@proeco/domain';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  const user = new User({ name: 'hoge' });
+  console.log(user);
+
   return (
     <div className={styles.container}>
       <Head>
