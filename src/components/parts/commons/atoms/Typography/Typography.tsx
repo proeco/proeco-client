a24ac8abf026fab954338290295da9c599b136dc
kanoi-y <furafuratonbo@gmail.com>
2021-10-01 20:44:58 +0900

    Typography.tsxを修正

--- src/components/parts/commons/atoms/Typography/Typography.tsx
+++ src/components/parts/commons/atoms/Typography/Typography.tsx
@@ -1,8 +1,12 @@
 import React, { VFC, ComponentProps } from 'react';
 import { Typography as MuiTypography } from '@mui/material';
 
-type Props = ComponentProps<typeof MuiTypography>;
+type Bold = {
+  bold?: boolean;
+};
 
-export const Typography: VFC<Props> = ({ ...rest }) => {
-  return <MuiTypography {...rest} />;
+type Props = ComponentProps<typeof MuiTypography> & Bold;
+
+export const Typography: VFC<Props> = ({ bold, ...rest }) => {
+  return <MuiTypography sx={{ textTransform: 'none', fontWeight: `${bold ? 'bold' : 'normal'}` }} {...rest} />;
 };
