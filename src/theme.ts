03e83ba7d0ef2fc4a11646ef0a0ca3bb3537fd3e
kanoi-y <furafuratonbo@gmail.com>
2021-10-01 10:42:19 +0900

    themeにTypographyを設定

--- src/theme.ts
+++ src/theme.ts
@@ -11,4 +11,33 @@ export const theme = createTheme({
       main: SECONDARY_COLOR,
     },
   },
+  typography: {
+    h1: {
+      fontSize: 48,
+    },
+    h2: {
+      fontSize: 32,
+    },
+    h3: {
+      fontSize: 24,
+    },
+    h4: {
+      fontSize: 20,
+    },
+    body1: {
+      fontSize: 16,
+    },
+    body2: {
+      fontSize: 14,
+    },
+    button: {
+      fontSize: 14,
+    },
+    caption: {
+      fontSize: 12,
+    },
+    overline: {
+      fontSize: 10,
+    },
+  },
 });
