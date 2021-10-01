bf6755ccca3e3b9cdd4d049a4fbb5e11b910aa24
kanoi-y <furafuratonbo@gmail.com>
2021-10-01 21:03:35 +0900

    修正

--- src/components/parts/commons/atoms/Typography/Typography.tsx
+++ src/components/parts/commons/atoms/Typography/Typography.tsx
@@ -1,5 +1,6 @@
 import React, { VFC, ComponentProps } from 'react';
 import { Typography as MuiTypography } from '@mui/material';
+import styled from 'styled-components';
 
 type Bold = {
   bold?: boolean;
@@ -8,5 +9,10 @@ type Bold = {
 type Props = ComponentProps<typeof MuiTypography> & Bold;
 
 export const Typography: VFC<Props> = ({ bold, ...rest }) => {
-  return <MuiTypography sx={{ textTransform: 'none', fontWeight: `${bold ? 'bold' : 'normal'}` }} {...rest} />;
+  return <StyledTypography bold={bold} {...rest} />;
 };
+
+const StyledTypography = styled(MuiTypography)<{ bold?: boolean }>`
+  text-transform: none;
+  font-weight: ${(props) => (props.bold ? 'bold' : 'normal')};
+`;
