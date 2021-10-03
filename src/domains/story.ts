0e661b2bff04f1c55f59f7af20244bce70b6e7a0
itizawa <itizawa.pen@gmail.com>
2021-10-03 20:44:34 +0900

    ✨ create Story domain

---
+++ src/domains/story.ts
@@ -0,0 +1,20 @@
+export class Story {
+  _id: string;
+  title: string;
+  emojiId: string;
+  description: string;
+  isPrivate: boolean;
+  createdUserId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  constructor(init: Story) {
+    this._id = init._id;
+    this.title = init.title;
+    this.emojiId = init.emojiId;
+    this.description = init.description;
+    this.isPrivate = init.isPrivate;
+    this.createdUserId = init.createdUserId;
+    this.createdAt = init.createdAt;
+    this.updatedAt = init.updatedAt;
+  }
+}
