60fd668e352cf2c5f8184837e8f34c920e50a7d4
kanoi-y <furafuratonbo@gmail.com>
2021-10-03 18:11:52 +0900

    修正

--- src/components/parts/commons/organisms/Modal/Modal.stories.tsx
+++ src/components/parts/commons/organisms/Modal/Modal.stories.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Box } from '@mui/system';
-import { Button, Typography } from '../../atoms';
+import { Typography } from '../../atoms';
 import { Modal } from './Modal';
 
 export default {
   title: 'parts/commons/organisms/Modal',
   component: Modal,
+  argTypes: { onClose: { action: 'closed' } },
 } as ComponentMeta<typeof Modal>;
 
 const Content = (
@@ -16,17 +17,24 @@ const Content = (
   </Box>
 );
 
-const Template: ComponentStory<typeof Modal> = () => {
-  const [openFlag, setOpenFlag] = useState(false);
-  const onCancel = () => {
-    setOpenFlag(false);
-  };
+const Template: ComponentStory<typeof Modal> = (args) => {
   return (
     <Box>
-      <Button onClick={() => setOpenFlag(true)}>モーダルを開く</Button>
-      <Modal title="ここにタイトルが入る" open={openFlag} onCancel={onCancel} content={Content}></Modal>
+      <Modal {...args}></Modal>
     </Box>
   );
 };
 
-export const Default = Template.bind({});
+export const OpenModal = Template.bind({});
+OpenModal.args = {
+  open: true,
+  content: Content,
+  title: 'ここにタイトルが入る',
+};
+
+export const CloseModal = Template.bind({});
+CloseModal.args = {
+  open: false,
+  content: Content,
+  title: 'ここにタイトルが入る',
+};
--- src/components/parts/commons/organisms/Modal/Modal.tsx
+++ src/components/parts/commons/organisms/Modal/Modal.tsx
@@ -5,34 +5,32 @@ import { Box } from '@mui/system';
 import { Typography } from '../../atoms/Typography';
 
 type Props = {
-  open: boolean; // Modal が開いているかどうか
-  title: string; // Modal のタイトル
-  content: JSX.Element; // Modal の中身
-  onCancel: () => void; // close するとき
+  open: boolean;
+  title: string;
+  content: JSX.Element;
+  onClose: () => void;
 };
 
-export const Modal: VFC<Props> = ({ open, title, content, onCancel }) => {
+export const Modal: VFC<Props> = ({ open, title, content, onClose }) => {
   return (
-    <StyledMuiModal open={open} onClose={onCancel}>
-      <StyledBox>
-        <StyledTypography variant="h4">{title}</StyledTypography>
-        {content}
+    <MuiModal open={open} onClose={onClose}>
+      <StyledBox width="600px">
+        <Box py="4px">
+          <StyledTypography variant="h4">{title}</StyledTypography>
+        </Box>
+        <Box p="20px">{content}</Box>
       </StyledBox>
-    </StyledMuiModal>
+    </MuiModal>
   );
 };
 
-const StyledMuiModal = styled(MuiModal)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
 const StyledBox = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   background-color: #fff;
-  width: 600px;
   border-radius: 4px;
-  padding-top: 8px;
 `;
 
 const StyledTypography = styled(Typography)`
