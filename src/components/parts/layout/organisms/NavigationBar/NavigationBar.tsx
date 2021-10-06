cd090fc5cb5de0086af7cfac722cbc6998c56be3
kanoi-y <furafuratonbo@gmail.com>
2021-10-06 22:22:32 +0900

    NavigationBar にロゴを表示

--- src/components/parts/layout/organisms/NavigationBar/NavigationBar.tsx
+++ src/components/parts/layout/organisms/NavigationBar/NavigationBar.tsx
@@ -1,12 +1,14 @@
 import { AppBar } from '@mui/material';
 import { memo, VFC } from 'react';
 import { styled } from '@mui/material/styles';
+import Image from 'next/image';
 
 import { UserIcon } from '~/components/domains/user/atoms/UserIcon';
 
 export const NavigationBar: VFC = memo(() => {
   return (
     <StyledAppBar>
+      <Image src="/images/Original.svg" alt="proecoのLogo" width={195} height={40} />
       <UserIcon />
     </StyledAppBar>
   );
@@ -16,6 +18,6 @@ const StyledAppBar = styled(AppBar)`
   background-color: ${(props) => props.theme.palette.primary.main};
   padding: 12px 20px;
   align-items: center;
-  justify-content: right;
+  justify-content: space-between;
   flex-direction: row;
 `;
