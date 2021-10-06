22bde058b3f39ed86962eab630653d39f5c60eb2
kanoi-y <furafuratonbo@gmail.com>
2021-10-06 13:34:55 +0900

    修正

--- src/components/domains/story/organisms/StoryStepper/StoryStepper.tsx
+++ src/components/domains/story/organisms/StoryStepper/StoryStepper.tsx
@@ -1,49 +1,10 @@
 import React, { ComponentProps, VFC } from 'react';
-import { styled } from '@mui/material/styles';
-import { Step, StepLabel, Stepper } from '@mui/material';
+import { Stepper } from '~/components/parts/commons/organisms/Stepper';
 
-import { TEXT_COLOR } from '~/constants/colors';
-
-type Props = ComponentProps<typeof Stepper>;
+type Props = Omit<ComponentProps<typeof Stepper>, 'steps'>;
 
 const steps = ['ストーリーを作る', 'ゴールを決める', 'タスクを作る'];
 
 export const StoryStepper: VFC<Props> = ({ ...rest }) => {
-  return (
-    <Stepper alternativeLabel {...rest}>
-      {steps.map((label) => (
-        <StyledStep key={label}>
-          <StyledStepLabel>{label}</StyledStepLabel>
-        </StyledStep>
-      ))}
-    </Stepper>
-  );
+  return <Stepper {...rest} steps={steps}></Stepper>;
 };
-
-const StyledStep = styled(Step)`
-  .MuiStepConnector-root {
-    top: 20px;
-  }
-`;
-
-const StyledStepLabel = styled(StepLabel)`
-  .MuiStepIcon-root {
-    width: 40px;
-    height: auto;
-  }
-  .MuiStepIcon-root.Mui-active {
-    color: ${(props) => props.theme.palette.secondary.main};
-    .MuiStepIcon-text {
-      fill: white;
-    }
-  }
-  .MuiStepIcon-root.Mui-completed {
-    color: ${(props) => props.theme.palette.secondary.main};
-  }
-  .MuiStepIcon-text {
-    fill: ${TEXT_COLOR};
-  }
-  .MuiStepLabel-label {
-    font-size: ${(props) => props.theme.typography.body1.fontSize}px;
-  }
-`;
