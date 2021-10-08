39f33d94763b41469d3b6f0af0eaa4675d17fa17
kanoi-y <furafuratonbo@gmail.com>
2021-10-08 21:56:16 +0900

    🎨 CreateNewStoryModalの中身を実装

--- src/components/domains/story/organisms/CreateNewStoryModal/CreateNewStoryModal.tsx
+++ src/components/domains/story/organisms/CreateNewStoryModal/CreateNewStoryModal.tsx
@@ -1,11 +1,33 @@
 import React, { VFC, ComponentProps } from 'react';
-import { styled } from '@mui/material/styles';
+import { Box } from '@mui/system';
 import { Modal } from '~/components/parts/commons/organisms/Modal';
+import { Button, Typography } from '~/components/parts/commons/atoms';
+import { TextField } from '~/components/parts/commons/atoms/TextField';
 
-type Props = ComponentProps<typeof Modal>;
+type Props = Omit<ComponentProps<typeof Modal>, 'title' | 'content'>;
+
+const title = '✨ ストーリーを作成する';
+
+const content = (
+  <>
+    <Box mb="16px">
+      <Typography mb="4px" variant="body1">
+        ストーリー名
+      </Typography>
+      <TextField fullWidth />
+    </Box>
+    <Box mb="16px">
+      <Typography mb="4px" variant="body1">
+        概要(任意)
+      </Typography>
+      <TextField fullWidth multiline rows={4} />
+    </Box>
+    <Box width="100%" textAlign="center">
+      <Button variant="contained">作る！</Button>
+    </Box>
+  </>
+);
 
 export const CreateNewStoryModal: VFC<Props> = ({ ...rest }) => {
-  return <StyledModal {...rest} />;
+  return <Modal content={content} title={title} {...rest} />;
 };
-
-const StyledModal = styled(Modal)``;
