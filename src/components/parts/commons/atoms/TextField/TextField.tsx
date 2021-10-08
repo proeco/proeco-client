7bc6369475ed95b665f5eda6376de47eaef49fff
kanoi-y <furafuratonbo@gmail.com>
2021-10-08 13:28:34 +0900

    ✨ TextField.stories.tsxを作成

--- src/components/parts/commons/atoms/TextField/TextField.tsx
+++ src/components/parts/commons/atoms/TextField/TextField.tsx
@@ -4,8 +4,19 @@ import { styled } from '@mui/material/styles';
 
 type Props = ComponentProps<typeof MuiTextField>;
 
-export const TextField: VFC<Props> = ({ variant = 'outlined', ...rest }) => {
-  return <StyledMuiTextField variant={variant} {...rest} />;
+export const TextField: VFC<Props> = ({ variant = 'outlined', hiddenLabel, ...rest }) => {
+  return <StyledMuiTextField variant={variant} hiddenLabel={hiddenLabel} {...rest} />;
 };
 
-const StyledMuiTextField = styled(MuiTextField)``;
+const StyledMuiTextField = styled(MuiTextField)`
+  .MuiOutlinedInput-root {
+    border-radius: 0;
+    padding: 8px;
+  }
+  .MuiOutlinedInput-input {
+    padding: 0px;
+  }
+  .MuiOutlinedInput-notchedOutline {
+    border: 1px solid #000;
+  }
+`;
