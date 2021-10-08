e3e6842ab5ccc654b653dddaa4b79b77b97676fc
kanoi-y <furafuratonbo@gmail.com>
2021-10-08 14:07:42 +0900

    🎨 修正

--- src/components/parts/commons/atoms/TextField/TextField.tsx
+++ src/components/parts/commons/atoms/TextField/TextField.tsx
@@ -1,22 +1,8 @@
 import React, { ComponentProps, VFC } from 'react';
 import { TextField as MuiTextField } from '@mui/material';
-import { styled } from '@mui/material/styles';
 
 type Props = ComponentProps<typeof MuiTextField>;
 
-export const TextField: VFC<Props> = ({ variant = 'outlined', hiddenLabel, ...rest }) => {
-  return <StyledMuiTextField variant={variant} hiddenLabel={hiddenLabel} {...rest} />;
+export const TextField: VFC<Props> = ({ variant = 'outlined', hiddenLabel = false, ...rest }) => {
+  return <MuiTextField variant={variant} hiddenLabel={hiddenLabel} {...rest} />;
 };
-
-const StyledMuiTextField = styled(MuiTextField)`
-  .MuiOutlinedInput-root {
-    border-radius: 0;
-    padding: 8px;
-  }
-  .MuiOutlinedInput-input {
-    padding: 0px;
-  }
-  .MuiOutlinedInput-notchedOutline {
-    border: 1px solid ${(props) => props.theme.palette.black.main};
-  }
-`;
