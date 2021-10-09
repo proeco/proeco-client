9d14a5acc8373c3324699253dedfac7843a11529
itizawa <itizawa.pen@gmail.com>
2021-10-09 23:54:32 +0900

    ✨ create ProecoOgpHead

---
+++ src/components/parts/layout/organisms/ProecoOgpHead/ProecoOgpHead.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import { VFC } from 'react';
+import { imagePath } from '~/constants/imagePath';
+
+type Props = {
+  siteName?: string;
+  title?: string;
+  url?: string;
+  image?: string;
+  description?: string;
+  keywords?: string[];
+};
+
+const DESCRIPTION = 'Proeco は、個人開発やチーム開発を応援するWEBアプリケーションです！';
+const KEYWORDS = 'プロセスエコノミー,個人開発';
+
+export const ProecoOgpHead: VFC<Props> = (props) => {
+  const { siteName, title, url, image, description, keywords = [] } = props;
+
+  return (
+    <>
+      <Head>
+        <title>{title || 'Proeco'}</title>
+        <meta name="description" content={description || DESCRIPTION} />
+        <meta name="keywords" content={keywords.length > 0 ? keywords?.join(',') : KEYWORDS} />
+        <meta property="og:site_name" content={siteName || 'Proeco'} />
+        <meta property="og:title" content={title || 'Proeco'} />
+        <meta property="og:url" content={url || 'https://www.proeco.app'} />
+        {/* TODO: create eye catch for all ogp */}
+        <meta property="og:image" content={image || `https://www.proeco.app/${imagePath.TWITTER_OGP}`} />
+        <meta property="og:description" content={description || DESCRIPTION} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title || 'Proeco'} />
+        <meta name="twitter:image" content={image || `https://www.proeco.app/${imagePath.TWITTER_OGP}`} />
+        <meta name="twitter:description" content={description || DESCRIPTION} />
+      </Head>
+    </>
+  );
+};
