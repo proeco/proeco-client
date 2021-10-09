90945e907df9474640333eed435d888265e79ddd
itizawa <itizawa.pen@gmail.com>
2021-10-10 00:36:08 +0900

    ✨ display title and button

--- src/pages/story/index.tsx
+++ src/pages/story/index.tsx
@@ -1,11 +1,26 @@
 import { NextPage } from 'next';
+
+import { Box } from '@mui/system';
+import { CreateOutlined as CreateOutlinedIcon } from '@mui/icons-material';
+
+import { Button, Typography } from '~/components/parts/commons/atoms';
 import { ProecoOgpHead } from '~/components/parts/layout/organisms/ProecoOgpHead';
 
 const StoryList: NextPage = () => {
   return (
     <>
       <ProecoOgpHead />
-      <p>Stroy!</p>
+      <Box p={5}>
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <Typography variant="h3" bold>
+            ストーリーリスト
+          </Typography>
+          <Button variant="contained" bold>
+            <CreateOutlinedIcon />
+            ストーリーを追加する
+          </Button>
+        </Box>
+      </Box>
     </>
   );
 };
