2f84a76fcecc2fcdd40020bd6a908f4ae6eb79dc
kanoi-y <furafuratonbo@gmail.com>
2021-10-10 23:14:59 +0900

    🔥 propsを削除

--- src/components/parts/layout/organisms/SideBar/SideBar.stories.tsx
+++ src/components/parts/layout/organisms/SideBar/SideBar.stories.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Box } from '@mui/system';
 import { SideBar } from '~/components/parts/layout/organisms/SideBar';
 
 export default {
   title: 'parts/layout/organisms/SideBar',
   component: SideBar,
-  argTypes: { openStoryModal: { action: 'open' } },
 } as ComponentMeta<typeof SideBar>;
 
-const Template: ComponentStory<typeof SideBar> = ({ openStoryModal }) => {
-  return (
-    <Box>
-      <SideBar openStoryModal={openStoryModal} />
-    </Box>
-  );
+const Template: ComponentStory<typeof SideBar> = () => {
+  return <SideBar />;
 };
 
 export const Default = Template.bind({});
--- src/components/parts/layout/organisms/SideBar/SideBar.tsx
+++ src/components/parts/layout/organisms/SideBar/SideBar.tsx
@@ -4,13 +4,12 @@ import { Box, styled } from '@mui/system';
 import { Add as AddIcon } from '@mui/icons-material';
 import { Button, Typography } from '~/components/parts/commons/atoms';
 
-type Props = {
-  openStoryModal: () => void;
-};
-
-export const SideBar: VFC<Props> = memo(({ openStoryModal }) => {
+export const SideBar: VFC = memo(() => {
+  const openStoryModal = () => {
+    console.log('open');
+  };
   return (
-    <Box width="280px" height="100vh" p="16px" boxSizing="border-box">
+    <Box width="280px" height="100vh" p="16px" boxSizing="border-box" bgcolor="#fff">
       <Box display="flex" width="100%" alignItems="center" justifyContent="space-between">
         <StyledTypography variant="body1">ストーリー</StyledTypography>
         <StyledButton variant="text">
