c07804dc98fd5f81df80962546f4f06decc01710
itizawa <itizawa.pen@gmail.com>
2021-10-10 18:20:56 +0900

    ✨ implement useStory

---
+++ src/stores/story/useStory.tsx
@@ -0,0 +1,19 @@
+import useSWR, { SWRResponse } from 'swr';
+
+import { restClient } from '~/utils/rest-client';
+import { Story } from '~/domains';
+
+/**
+ * ストーリーを取得するSWR
+ * @returns data ストーリー
+ * @returns isValidating 取得中を表す boolean
+ * @returns error エラー
+ * @returns mutate データの更新関数
+ */
+export const useStory = (id: string, fallbackData: Story): SWRResponse<Story, Error> => {
+  return useSWR(`/stories/${id}`, (endpoint: string) => restClient.apiGet(endpoint).then((result) => result.data), {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: true,
+    fallbackData,
+  });
+};
