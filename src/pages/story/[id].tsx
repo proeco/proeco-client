0c71c5d7e678c4e1fac8fe176093d1f9705b7c66
itizawa <itizawa.pen@gmail.com>
2021-10-10 18:13:42 +0900

    ✨ implement StoryPage

---
+++ src/pages/story/[id].tsx
@@ -0,0 +1,46 @@
+import { GetServerSideProps, NextPage } from 'next';
+import { Box } from '@mui/system';
+
+import { restClient } from '~/utils/rest-client';
+import { Story } from '~/domains/story';
+
+import { Typography } from '~/components/parts/commons/atoms';
+import { ProecoOgpHead } from '~/components/parts/layout/organisms/ProecoOgpHead';
+
+type Props = {
+  story?: Story;
+};
+
+const StoryPage: NextPage<Props> = ({ story }) => {
+  console.log(story);
+
+  if (!story) {
+    return null;
+  }
+
+  return (
+    <>
+      <ProecoOgpHead />
+      <Box p={5} mx="auto" maxWidth="1200px">
+        <Box mb={2} display="flex" alignItems="center" justifyContent="space-between">
+          <Typography variant="h2" bold>
+            {story.title}
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+
+  try {
+    const { data: story } = await restClient.apiGet(`/stories/${id}`);
+    return { props: { story } };
+  } catch (error) {
+    return { props: {} };
+  }
+};
+
+export default StoryPage;
