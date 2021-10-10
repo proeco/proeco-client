f94a201a7720309429096a49c86613601daeea79
kanoi-y <furafuratonbo@gmail.com>
2021-10-10 16:42:21 +0900

    🎨 propsを追加

--- src/components/parts/layout/organisms/SideBar/SideBar.tsx
+++ src/components/parts/layout/organisms/SideBar/SideBar.tsx
@@ -3,10 +3,14 @@ import { memo, VFC } from 'react';
 import { Box } from '@mui/system';
 import { Add as AddIcon } from '@mui/icons-material';
 
-export const SideBar: VFC = memo(() => {
+type Props = {
+  openStoryModal: () => void;
+};
+
+export const SideBar: VFC<Props> = memo(({ openStoryModal }) => {
   return (
     <Box>
-      <AddIcon width="20px" height="20px" />
+      <AddIcon width="20px" height="20px" onClick={openStoryModal} />
     </Box>
   );
 });
