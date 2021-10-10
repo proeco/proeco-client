d4172a8859f29999c43b12c4a1bcb04dfd34751a
itizawa <itizawa.pen@gmail.com>
2021-10-10 23:28:35 +0900

    ✨ display DashboardModals at _app.tsx

---
+++ src/components/parts/layout/organisms/DashboardModals/DashboardModals.tsx
@@ -0,0 +1,6 @@
+import { VFC } from 'react';
+import { CreateNewStoryModal } from '~/components/domains/story/organisms';
+
+export const DashboardModals: VFC = () => {
+  return <CreateNewStoryModal />;
+};
