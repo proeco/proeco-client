1587689eca9830348f3daaafb725de0b49092e2c
itizawa <itizawa.pen@gmail.com>
2021-10-10 18:25:44 +0900

    ✨ use swr

--- src/pages/story/[id].tsx
+++ src/pages/story/[id].tsx
@@ -1,4 +1,6 @@
 import { GetServerSideProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+
 import { Box } from '@mui/system';
 
 import { restClient } from '~/utils/rest-client';
@@ -6,13 +8,17 @@ import { Story } from '~/domains/story';
 
 import { Typography } from '~/components/parts/commons/atoms';
 import { ProecoOgpHead } from '~/components/parts/layout/organisms/ProecoOgpHead';
+import { useStory } from '~/stores/story/useStory';
 
 type Props = {
-  story?: Story;
+  storyFromServerSide?: Story;
 };
 
-const StoryPage: NextPage<Props> = ({ story }) => {
-  console.log(story);
+const StoryPage: NextPage<Props> = ({ storyFromServerSide }) => {
+  const router = useRouter();
+
+  const { id } = router.query;
+  const { data: story } = useStory(id as string, storyFromServerSide);
 
   if (!story) {
     return null;
@@ -38,7 +44,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   try {
     const { data: story } = await restClient.apiGet(`/stories/${id}`);
-    return { props: { story } };
+    return { props: { storyFromServerSide: story } };
   } catch (error) {
     return { props: {} };
   }
