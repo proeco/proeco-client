fe656cb6813d6fa8d29474e2a230b2adf6cc20e1
kanoi-y <furafuratonbo@gmail.com>
2021-10-10 16:21:47 +0900

    💄 AddIconを追加

--- src/components/parts/layout/organisms/SideBar/SideBar.tsx
+++ src/components/parts/layout/organisms/SideBar/SideBar.tsx
@@ -1,6 +1,12 @@
 import { memo, VFC } from 'react';
+
 import { Box } from '@mui/system';
+import { Add as AddIcon } from '@mui/icons-material';
 
 export const SideBar: VFC = memo(() => {
-  return <Box></Box>;
+  return (
+    <Box>
+      <AddIcon width="20px" height="20px" />
+    </Box>
+  );
 });
