e4cbb7c9853f4823af4c9aa02cc12e4dc73a66c6
itizawa <itizawa.pen@gmail.com>
2021-10-10 23:46:08 +0900

    ✨ add mutate

--- src/components/domains/story/organisms/CreateNewStoryModal/CreateNewStoryModal.stories.tsx
+++ src/components/domains/story/organisms/CreateNewStoryModal/CreateNewStoryModal.stories.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Box } from '@mui/system';
 import { CreateNewStoryModal } from './CreateNewStoryModal';
+import { useIsOpenCreateNewStoryModal } from '~/stores/modal/useIsOpenCreateNewStory';
 
 export default {
   title: 'domains/story/organisms/CreateNewStoryModal',
@@ -10,6 +11,12 @@ export default {
 } as ComponentMeta<typeof CreateNewStoryModal>;
 
 const Template: ComponentStory<typeof CreateNewStoryModal> = () => {
+  const { mutate } = useIsOpenCreateNewStoryModal();
+
+  useEffect(() => {
+    mutate(true);
+  }, []);
+
   return (
     <Box>
       <CreateNewStoryModal />
