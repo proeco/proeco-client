e49f57fb1da1108657e8400eb1b0a6f45f8b145c
kanoi-y <furafuratonbo@gmail.com>
2021-10-10 17:30:54 +0900

    💄 ボタン周りの見た目を調整

--- src/components/parts/layout/organisms/SideBar/SideBar.tsx
+++ src/components/parts/layout/organisms/SideBar/SideBar.tsx
@@ -1,8 +1,8 @@
 import { memo, VFC } from 'react';
 
-import { Box } from '@mui/system';
+import { Box, styled } from '@mui/system';
 import { Add as AddIcon } from '@mui/icons-material';
-import { Button } from '~/components/parts/commons/atoms';
+import { Button, Typography } from '~/components/parts/commons/atoms';
 
 type Props = {
   openStoryModal: () => void;
@@ -10,10 +10,31 @@ type Props = {
 
 export const SideBar: VFC<Props> = memo(({ openStoryModal }) => {
   return (
-    <Box>
-      <Button variant="text">
-        <AddIcon onClick={openStoryModal} />
-      </Button>
+    <Box width="280px" height="100vh" p="16px" boxSizing="border-box">
+      <Box display="flex" width="100%" alignItems="center" justifyContent="space-between">
+        <StyledTypography variant="body1">ストーリー</StyledTypography>
+        <StyledButton variant="text">
+          <StyledAddIcon onClick={openStoryModal} />
+        </StyledButton>
+      </Box>
     </Box>
   );
 });
+
+const StyledTypography = styled(Typography)`
+  color: ${(props) => props.theme.palette.textColor.light};
+`;
+
+const StyledButton = styled(Button)`
+  width: 20px;
+  height: 20px;
+  min-width: unset;
+  padding: 0;
+  margin-right: 8px;
+`;
+
+const StyledAddIcon = styled(AddIcon)`
+  width: 100%;
+  height: auto;
+  color: ${(props) => props.theme.palette.textColor.light};
+`;
