19a27d5970fe0d98482c6c56288b9b882e69f32a
kanoi-y <furafuratonbo@gmail.com>
2021-10-11 19:06:13 +0900

    🎨 SideBarListItemの見た目を調整

--- src/components/parts/commons/atoms/SideBarListItem/SideBarListItem.stories.tsx
+++ src/components/parts/commons/atoms/SideBarListItem/SideBarListItem.stories.tsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { Box } from '@mui/system';
-import { ListItemIcon, ListItemText } from '@mui/material';
+import { styled } from '@mui/system';
+import { List } from '@mui/material';
 import { DashboardOutlined as DashboardOutlinedIcon } from '@mui/icons-material';
+
 import { SideBarListItem } from '~/components/parts/commons/atoms/SideBarListItem';
+import { Typography } from '~/components/parts/commons/atoms';
 
 export default {
   title: 'parts/commons/atoms/SideBarListItem',
   component: SideBarListItem,
 } as ComponentMeta<typeof SideBarListItem>;
 
-const Template: ComponentStory<typeof SideBarListItem> = () => {
+const Template: ComponentStory<typeof SideBarListItem> = ({ typography, selected }) => {
   return (
-    <Box>
-      <SideBarListItem>
-        <ListItemIcon>
-          <DashboardOutlinedIcon />
-        </ListItemIcon>
-        <ListItemText primary="ダッシュボード" />
+    <StyledList>
+      <SideBarListItem typography={typography} selected={selected}>
+        <StyledDashboardOutlinedIcon fontSize="small" />
       </SideBarListItem>
-    </Box>
+    </StyledList>
   );
 };
 
-export const DefaultCard = Template.bind({});
+const StyledList = styled(List)`
+  width: 280px;
+`;
+
+const StyledDashboardOutlinedIcon = styled(DashboardOutlinedIcon)`
+  color: ${(props) => props.theme.palette.textColor.main};
+`;
+
+export const Default = Template.bind({});
+Default.args = {
+  typography: <Typography variant="body1">ダッシュボード</Typography>,
+  selected: false,
+};
+
+export const Selected = Template.bind({});
+Selected.args = {
+  typography: <Typography variant="body1">ダッシュボード</Typography>,
+  selected: true,
+};
--- src/components/parts/commons/atoms/SideBarListItem/SideBarListItem.tsx
+++ src/components/parts/commons/atoms/SideBarListItem/SideBarListItem.tsx
@@ -1,8 +1,45 @@
 import React, { VFC, ComponentProps } from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
+import { styled } from '@mui/system';
+import { ListItemIcon, ListItemText } from '@mui/material';
 
-type Props = ComponentProps<typeof ListItemButton>;
-
-export const SideBarListItem: VFC<Props> = ({ children }) => {
-  return <ListItemButton>{children}</ListItemButton>;
+type SideBarListItemType = {
+  typography: JSX.Element;
 };
+
+type Props = ComponentProps<typeof ListItemButton> & SideBarListItemType;
+
+export const SideBarListItem: VFC<Props> = ({ children, selected, typography }) => {
+  return (
+    <StyledListItemButton selected={selected}>
+      <StyledListItemIcon>{children}</StyledListItemIcon>
+      <StyledListItemText disableTypography primary={typography} />
+    </StyledListItemButton>
+  );
+};
+
+const StyledListItemButton = styled(ListItemButton)`
+  padding: 8px;
+  border-radius: 4px;
+  &.Mui-selected {
+    background-color: ${(props) => props.theme.palette.primary.main};
+    .MuiSvgIcon-root {
+      color: #fff;
+    }
+    .MuiTypography-root {
+      color: #fff;
+    }
+  }
+  &:hover {
+    background-color: #e3f2fd;
+  }
+`;
+
+const StyledListItemIcon = styled(ListItemIcon)`
+  min-width: unset;
+  margin-right: 8px;
+`;
+
+const StyledListItemText = styled(ListItemText)`
+  margin: 0;
+`;
