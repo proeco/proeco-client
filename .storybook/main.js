782122cd9120f8fd185b294b9e6f9c26cc548b6e
itizawa <itizawa.pen@gmail.com>
2021-10-12 23:14:45 +0900

    👷 update storybook system

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const path = require('path')
+var webpack = require('webpack');
 
 module.exports = {
   "stories": [
@@ -22,6 +23,15 @@ module.exports = {
     config.resolve.alias = {
       "~": path.resolve(__dirname, "../src")
     }
+    config.plugins.push(new webpack.DefinePlugin({
+      'process.env.__NEXT_IMAGE_OPTS': JSON.stringify({
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        domains: [],
+        path: '/',
+        loader: 'default',
+      }),
+    }));
     return config
   }
 }
