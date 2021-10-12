ee6ac715f773efd51824ebc8ae7d53d4fa01639f
itizawa <itizawa.pen@gmail.com>
2021-10-13 01:08:40 +0900

    💄 update storybook

--- src/components/domains/story/organisms/CreateNewStoryModal/CreateNewStoryModal.stories.tsx
+++ src/components/domains/story/organisms/CreateNewStoryModal/CreateNewStoryModal.stories.tsx
@@ -1,28 +1,33 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Box } from '@mui/system';
-import { CreateNewStoryModal } from './CreateNewStoryModal';
-import { useIsOpenCreateNewStoryModal } from '~/stores/modal/useIsOpenCreateNewStory';
+import { Component } from './CreateNewStoryModal';
 
 export default {
   title: 'domains/story/organisms/CreateNewStoryModal',
-  component: CreateNewStoryModal,
-} as ComponentMeta<typeof CreateNewStoryModal>;
+  component: Component,
+  argTypes: {
+    onChangeTitle: { action: 'onChangeTitle' },
+    onChangeDescription: { action: 'onChangeDescription' },
+    onClickCreateNewStoryButton: { action: 'onClickCreateNewStoryButton' },
+    onSelectEmoji: { action: 'onSelectEmoji' },
+    onCloseModal: { action: 'onCloseModal' },
+  },
+} as ComponentMeta<typeof Component>;
 
-const Template: ComponentStory<typeof CreateNewStoryModal> = () => {
-  const { mutate } = useIsOpenCreateNewStoryModal();
-
-  useEffect(() => {
-    mutate(true);
-  }, []);
-
+const Template: ComponentStory<typeof Component> = (args) => {
   return (
     <Box>
-      <CreateNewStoryModal />
+      <Component {...args} />
     </Box>
   );
 };
 
 export const OpenModal = Template.bind({});
-OpenModal.args = {};
+OpenModal.args = {
+  isOpen: true,
+  title: 'ここにタイトル',
+  description: 'ここに説明',
+  emojiId: 'tada',
+};
