2d13844cb355d0a5d0b6ae1e8135384d62a5b5f5
kanoi-y <furafuratonbo@gmail.com>
2021-10-13 11:03:09 +0900

    ✨Paper.stories.tsxの作成

--- src/components/parts/commons/atoms/Paper/Paper.tsx
+++ src/components/parts/commons/atoms/Paper/Paper.tsx
@@ -2,17 +2,18 @@ import React, { VFC, ComponentProps } from 'react';
 import { Paper as MuiPaper } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-type Card = {
-  square?: boolean;
+type Paper = {
+  padding?: number;
 };
 
-type Props = ComponentProps<typeof MuiPaper> & Card;
+type Props = ComponentProps<typeof MuiPaper> & Paper;
 
-export const Paper: VFC<Props> = ({ square = false, ...rest }) => {
-  return <StyledMuiPaper square={square} {...rest} />;
+export const Paper: VFC<Props> = ({ padding = 20, ...rest }) => {
+  return <StyledMuiPaper padding={padding} {...rest} />;
 };
 
-const StyledMuiPaper = styled(MuiPaper)<{ square: boolean }>`
-  border-radius: ${(props) => (props.square ? '0px' : '4px')};
+const StyledMuiPaper = styled(MuiPaper)<{ padding: number }>`
+  width: fit-content;
+  padding: ${(props) => props.padding}px;
   box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.25);
 `;
