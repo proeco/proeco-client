ed725cc0ee2c01dbd210690236e7af1403198896
kanoi-y <furafuratonbo@gmail.com>
2021-10-15 13:50:31 +0900

    🔨 _app.tsxにSnackbarProviderを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
+import { SnackbarProvider } from "notistack";
 import { ThemeProvider as MaterialThemeProvider } from '@mui/material/styles';
 
 import 'modern-css-reset/dist/reset.min.css';
@@ -11,12 +12,17 @@ import { DashboardModals } from '~/components/parts/layout/organisms/DashboardMo
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <MaterialThemeProvider theme={theme}>
-      <Provider options={{ clientMaxAge: 0, keepAlive: 0 }} session={pageProps.session}>
+      <SnackbarProvider>
+      <Provider
+        options={{ clientMaxAge: 0, keepAlive: 0 }}
+        session={pageProps.session}
+      >
         <NavigationBar />
         <Component {...pageProps} />
         {/* TODO: Dashboard レイアウトができたら移動する */}
         <DashboardModals />
       </Provider>
+      </SnackbarProvider>
     </MaterialThemeProvider>
   );
 }
