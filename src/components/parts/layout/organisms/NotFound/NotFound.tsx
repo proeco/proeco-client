a5eb6d8f877b6098c165eab855805a07ba2e95ac
kanoi-y <furafuratonbo@gmail.com>
2021-10-16 22:40:38 +0900

    💄 NotFound.tsxの見た目を実装

--- src/components/parts/layout/organisms/NotFound/NotFound.tsx
+++ src/components/parts/layout/organisms/NotFound/NotFound.tsx
@@ -1,6 +1,31 @@
-import { Box } from '@mui/system';
 import { VFC } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { styled } from '@mui/material/styles';
 
-export const NotFound: VFC = () => {
-  return <Box></Box>;
+import { Stack } from '@mui/material';
+import { Button, Typography } from '~/components/parts/commons/atoms';
+
+type Props = {
+  message?: string;
 };
+
+export const NotFound: VFC<Props> = ({ message = 'ページが見つかりませんでした' }) => {
+  return (
+    <StyledStack spacing="20px" alignItems="center">
+      <Typography variant="h3">{message}</Typography>
+      <Image src="/images/Error_404_SVG.svg" alt="NotFound" width={500} height={315} />
+      <Link href="/">
+        <a>
+          <Button variant="contained">Topページに戻る</Button>
+        </a>
+      </Link>
+    </StyledStack>
+  );
+};
+
+const StyledStack = styled(Stack)`
+  a {
+    text-decoration: none;
+  }
+`;
