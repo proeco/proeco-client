7814bf93cc00fd1a2ae03d184ef61981331b8a53
itizawa <itizawa.pen@gmail.com>
2021-10-17 19:58:02 +0900

    ✨ create LoginRequiredWrapper

---
+++ src/components/parts/authentication/LoginRequiredWrapper.tsx
@@ -0,0 +1,33 @@
+import { useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { FC, useEffect } from 'react';
+import Loader from 'react-loader-spinner';
+import { COLORS } from '~/constants';
+
+export const LoginRequiredWrapper: FC = ({ children }) => {
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && session == null) {
+      router.push('/login?isRedirect=true');
+    }
+  }, [loading, session]);
+
+  if (typeof window !== 'undefined' && loading)
+    return (
+      <div className="text-center pt-5">
+        <Loader type="Oval" color={COLORS.PRIMARY} height={100} width={100} />
+      </div>
+    );
+
+  if (session != null) {
+    return <>{children}</>;
+  }
+
+  return (
+    <div className="text-center pt-5">
+      <Loader type="Oval" color={COLORS.PRIMARY} height={100} width={100} />
+    </div>
+  );
+};
