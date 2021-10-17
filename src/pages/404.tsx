0722734ed45c7ec953c35a0c35d2d424d315fde6
kanoi-y <furafuratonbo@gmail.com>
2021-10-17 19:39:28 +0900

    ✨ 404.tsxを作成

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { ProecoOgpHead } from '~/components/parts/layout/organisms/ProecoOgpHead';
+
+const Custom404: NextPage = () => {
+  return (
+    <>
+      <ProecoOgpHead />
+    </>
+  );
+};
+
+export default Custom404;
