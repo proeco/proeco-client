c7f88567cf67d0f057315b481352c9a8590cb7b5
Steven Fukase <fukasesteven@gmail.com>
2021-10-20 15:50:25 +0900

    storybook fix

--- src/components/parts/commons/organisms/Modal/Modal.stories.tsx
+++ src/components/parts/commons/organisms/Modal/Modal.stories.tsx
@@ -24,10 +24,33 @@ const Template: ComponentStory<typeof Modal> = (args) => {
     </Box>
   );
 };
-
 export const OpenModal = Template.bind({});
+export const SmallModal = Template.bind({});
+export const MediumModal = Template.bind({});
+export const LargeModal = Template.bind({});
+
 OpenModal.args = {
+  open: true,
+  content: Content,
+  title: 'ここにタイトルが入る',
+};
+
+SmallModal.args = {
+  size: 'small',
+  open: true,
+  content: Content,
+  title: 'ここにタイトルが入る',
+};
+
+MediumModal.args = {
   size: 'medium',
+  open: true,
+  content: Content,
+  title: 'ここにタイトルが入る',
+};
+
+LargeModal.args = {
+  size: 'large',
   open: true,
   content: Content,
   title: 'ここにタイトルが入る',
