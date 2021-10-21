ca5534aba4c159a9bcccf3f0f8713ac182cc5d2e
itizawa <itizawa.pen@gmail.com>
2021-10-21 22:37:51 +0900

    💄 update style

--- src/components/parts/authentication/LoginModal/LoginModal.tsx
+++ src/components/parts/authentication/LoginModal/LoginModal.tsx
@@ -16,17 +16,17 @@ export const LoginModal: FC<LoginModalProps> = ({ isOpen, onClose, onClickSignIn
   const content = (
     <>
       <Box px={1}>
-        <Box width="100%" height="4rem" mx="auto" position="relative">
+        <Box width="400px" height="60px" mx="auto" position="relative">
           <Image src={IMAGE_PATH.LOGO} layout="fill" objectFit="contain" />
         </Box>
 
         <Box my="20px">
-          <Typography color="textColor.light">Proeco はプロダクト開発を支援するプラットフォームです。</Typography>
+          <Typography color="textColor.light">Proeco はプロダクト開発を支援するプラットフォームです</Typography>
           <Typography color="textColor.light">あなたの素敵なアイデアを実現しましょう</Typography>
         </Box>
 
         <StyledLoginButtonWrapper my="20px" display="flex" justifyContent="center" role="button" onClick={onClickSignInButton}>
-          <Image src={IMAGE_PATH.SIGN_IN_GOOGLE} height={46} width={191} />
+          <Image src={IMAGE_PATH.SIGN_IN_GOOGLE} height={50} width={190} />
         </StyledLoginButtonWrapper>
 
         <Typography my="20px" color="textColor.light">
@@ -36,7 +36,7 @@ export const LoginModal: FC<LoginModalProps> = ({ isOpen, onClose, onClickSignIn
     </>
   );
 
-  return <Modal open={isOpen} size="small" title="✨ Proecoへようこそ！ ✨" content={content} onClose={onClose} />;
+  return <Modal open={isOpen} size="small" title="✨ Proecoへようこそ ✨" content={content} onClose={onClose} />;
 };
 
 const StyledLoginButtonWrapper = styled(Box)`
