4bf59eb01f5422da94b9ca7aef2563921866b57b
itizawa <itizawa.pen@gmail.com>
2021-10-21 23:03:10 +0900

    🐛 fix

--- src/components/domains/story/organisms/DeleteStoryModal/DeleteStoryModal.tsx
+++ src/components/domains/story/organisms/DeleteStoryModal/DeleteStoryModal.tsx
@@ -18,11 +18,9 @@ export const DeleteStoryModal: FC<DeleteStoryModalProps> = ({ onClose, onDeleteS
       <Box>
         <Typography>ストーリー名</Typography>
         <Box display="flex" alignItems="center">
-          {story?.emojiId && (
-            <Box mr="8px">
-              <Emoji emoji={story?.emojiId} size={40} />
-            </Box>
-          )}
+          <Box mr="8px">
+            <Emoji emoji={story?.emojiId || ''} size={40} />
+          </Box>
           <Typography variant="h2" bold>
             {story?.title}
           </Typography>
