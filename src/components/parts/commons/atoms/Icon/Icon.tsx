4fbb580ee9aaa2f7ead43913cac6b18df8fc390d
kanoi-y <furafuratonbo@gmail.com>
2021-10-22 15:38:52 +0900

    🔨 IconMapをBoxでwarpした

--- src/components/parts/commons/atoms/Icon/Icon.tsx
+++ src/components/parts/commons/atoms/Icon/Icon.tsx
@@ -1,16 +1,20 @@
-import React, { VFC } from 'react';
-import { MoreVert, Update, Delete } from '@mui/icons-material';
+import React, { VFC, ComponentProps } from 'react';
+import { MoreVert, Update, Delete, Logout } from '@mui/icons-material';
+import { Box } from '@mui/system';
 
 const IconMap = {
-  MoreVert: <MoreVert />,
-  Update: <Update />,
-  Delete: <Delete />,
+  MoreVert: <MoreVert color="inherit" sx={{ width: '100%', height: 'auto', display: 'block' }} />,
+  Update: <Update color="inherit" sx={{ width: '100%', height: 'auto', display: 'block' }} />,
+  Delete: <Delete color="inherit" sx={{ width: '100%', height: 'auto', display: 'block' }} />,
+  Logout: <Logout color="inherit" sx={{ width: '100%', height: 'auto', display: 'block' }} />,
 };
 
-type Props = {
+type IconType = {
   icon: keyof typeof IconMap;
 };
 
-export const Icon: VFC<Props> = ({ icon }) => {
-  return IconMap[icon];
+type Props = ComponentProps<typeof Box> & IconType;
+
+export const Icon: VFC<Props> = ({ icon, ...rest }) => {
+  return <Box {...rest}>{IconMap[icon]}</Box>;
 };
