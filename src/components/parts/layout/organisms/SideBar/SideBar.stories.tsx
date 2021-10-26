d6e4b586c0e459b6bbd905a6cf990b9bad31bf7d
kanoi-y <furafuratonbo@gmail.com>
2021-10-26 20:11:35 +0900

    💄 SideBarにstoryを表示

--- src/components/parts/layout/organisms/SideBar/SideBar.stories.tsx
+++ src/components/parts/layout/organisms/SideBar/SideBar.stories.tsx
@@ -3,12 +3,14 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Component } from './SideBar';
 
-import { createMockUser } from '~/mock';
+import { createMockStory, createMockUser } from '~/mock';
 
 export default {
   title: 'parts/layout/organisms/SideBar',
   component: Component,
-  argTypes: { openCreateStoryModal: { action: 'openCreateStoryModal' } },
+  argTypes: {
+    openCreateStoryModal: { action: 'openCreateStoryModal' },
+  },
 } as ComponentMeta<typeof Component>;
 
 const Template: ComponentStory<typeof Component> = ({ ...rest }) => {
@@ -19,4 +21,10 @@ export const Default = Template.bind({});
 
 Default.args = {
   currentUser: createMockUser({ image: 'https://itizawa-tech.growi.cloud/attachment/616289c6c4e99c0051b30574' }),
+  docs: [
+    createMockStory({
+      title: '️Proecoを開発する',
+      emojiId: 'tada',
+    }),
+  ],
 };
--- src/components/parts/layout/organisms/SideBar/SideBar.tsx
+++ src/components/parts/layout/organisms/SideBar/SideBar.tsx
@@ -1,37 +1,61 @@
 import { memo, VFC } from 'react';
 import { Box, styled } from '@mui/system';
 
+import { Link } from '@mui/material';
 import { useIsOpenCreateNewStoryModal } from '~/stores/modal/useIsOpenCreateNewStory';
 import { useCurrentUser } from '~/stores/user/useCurrentUser';
 
-import { Typography, SideBarListItem, Icon } from '~/components/parts/commons/atoms';
+import { Typography, SideBarListItem, Icon, Emoji } from '~/components/parts/commons/atoms';
 import { IconButton } from '~/components/parts/commons/organisms/IconButton';
 import { UserIcon } from '~/components/domains/user/atoms';
 
-import { User } from '~/domains';
+import { Story, User } from '~/domains';
+
+import { URLS } from '~/constants/urls';
+import { useStories } from '~/stores/story';
 
 type Props = {
   currentUser?: User;
   openCreateStoryModal: () => void;
+  docs: Story[];
 };
 
-export const Component: VFC<Props> = memo(({ currentUser, openCreateStoryModal }) => {
+export const Component: VFC<Props> = memo(({ currentUser, openCreateStoryModal, docs }) => {
   return (
-    <Box width="280px" height="100vh" p="16px" boxSizing="border-box" bgcolor="#fff">
+    <StyledSideBarWrapper width="280px" height="100vh" p="16px">
       <StyledUserIconWrapper pb="16px">
         <UserIcon size="large" imagePath={currentUser?.image} userId={currentUser?._id} isLink />
         <Typography variant="h3">{currentUser?.name}</Typography>
       </StyledUserIconWrapper>
-      <SideBarListItem icon={<Icon icon="DashboardOutlined" width="20px" color="textColor.main" />}>
-        <Typography variant="body1">ダッシュボード</Typography>
-      </SideBarListItem>
-      <Box display="flex" width="100%" alignItems="center" justifyContent="space-between">
+      <Box p="12px 0 24px">
+        <Link href={URLS.DASHBOARD} underline="none">
+          <SideBarListItem icon={<Icon icon="DashboardOutlined" width="20px" color="textColor.main" />}>
+            <Typography variant="body1">ダッシュボード</Typography>
+          </SideBarListItem>
+        </Link>
+      </Box>
+      <Box display="flex" width="100%" pb="12px" alignItems="center" justifyContent="space-between">
         <StyledTypography variant="body1">ストーリー</StyledTypography>
         <StyledIconButton icon="Add" width={18} aria-label="open" size="small" onClick={openCreateStoryModal} />
       </Box>
-    </Box>
+      <Box>
+        {docs.map((story) => (
+          <Link key={story._id} href={'/story/' + story._id} underline="none">
+            <SideBarListItem icon={<Emoji emojiId={story.emojiId} size={20} />}>
+              <Typography variant="body1">{story.title}</Typography>
+            </SideBarListItem>
+          </Link>
+        ))}
+      </Box>
+    </StyledSideBarWrapper>
   );
 });
+
+const StyledSideBarWrapper = styled(Box)`
+  box-sizing: border-box;
+  background-color: #fff;
+  border-right: 1px solid ${(props) => props.theme.palette.borderColor.main};
+`;
 
 const StyledTypography = styled(Typography)`
   color: ${(props) => props.theme.palette.textColor.light};
@@ -52,9 +76,17 @@ const StyledUserIconWrapper = styled(Box)`
 export const SideBar: VFC = memo(() => {
   const { data: currentUser } = useCurrentUser();
 
+  const { data: stories } = useStories({
+    userId: currentUser?._id,
+    page: 1,
+    limit: 10,
+  });
+
+  const docs = stories ? stories.docs : [];
+
   const { mutate: mutateIsOpenCreateNewStoryModal } = useIsOpenCreateNewStoryModal();
   const openCreateStoryModal = () => {
     mutateIsOpenCreateNewStoryModal(true);
   };
-  return <Component currentUser={currentUser} openCreateStoryModal={openCreateStoryModal} />;
+  return <Component currentUser={currentUser} openCreateStoryModal={openCreateStoryModal} docs={docs} />;
 });
