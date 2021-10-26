f2c0452382295ff7421fc78699e62fb1b7be2440
kanoi-y <furafuratonbo@gmail.com>
2021-10-26 11:44:28 +0900

    🔨 storybookにmockを追加

--- src/components/parts/layout/organisms/SideBar/SideBar.stories.tsx
+++ src/components/parts/layout/organisms/SideBar/SideBar.stories.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { SideBar } from '~/components/parts/layout/organisms/SideBar';
+import { Component } from './SideBar';
+
+import { createMockUser } from '~/mock';
 
 export default {
   title: 'parts/layout/organisms/SideBar',
-  component: SideBar,
-} as ComponentMeta<typeof SideBar>;
+  component: Component,
+  argTypes: { openCreateStoryModal: { action: 'openCreateStoryModal' } },
+} as ComponentMeta<typeof Component>;
 
-const Template: ComponentStory<typeof SideBar> = () => {
-  return <SideBar />;
+const Template: ComponentStory<typeof Component> = ({ ...rest }) => {
+  return <Component {...rest} />;
 };
 
 export const Default = Template.bind({});
+
+Default.args = {
+  currentUser: createMockUser({ image: 'https://itizawa-tech.growi.cloud/attachment/616289c6c4e99c0051b30574' }),
+};
