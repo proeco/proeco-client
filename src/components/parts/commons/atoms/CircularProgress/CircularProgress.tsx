ad27b385ba0a249359adba009edfd79d263e8572
stevenfukase <fukasesteven@gmail.com>
2021-10-29 14:42:36 +0900

    FB fix

--- src/components/parts/commons/atoms/CircularProgress/CircularProgress.tsx
+++ src/components/parts/commons/atoms/CircularProgress/CircularProgress.tsx
@@ -1,8 +1,7 @@
 import React, { ComponentProps, VFC } from 'react';
 import MuiCircularProgress from '@mui/material/CircularProgress';
 
-type CircularProgress = {};
-type Props = ComponentProps<typeof MuiCircularProgress> & CircularProgress;
+type Props = ComponentProps<typeof MuiCircularProgress>;
 
 export const CircularProgress: VFC<Props> = (props) => {
   return <MuiCircularProgress {...props} />;
