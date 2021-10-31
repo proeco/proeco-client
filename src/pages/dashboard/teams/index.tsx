b1a19544a172c5b401169730b2be956493517287
itizawa <itizawa.pen@gmail.com>
2021-10-31 17:48:05 +0900

    ✨ display button

--- src/pages/dashboard/teams/index.tsx
+++ src/pages/dashboard/teams/index.tsx
@@ -1,13 +1,22 @@
+import { Box } from '@mui/system';
 import { ReactNode } from 'react';
+import { Button, Link } from '~/components/parts/commons';
 import { DashBoardLayout } from '~/components/parts/layout/DashboardLayout';
 import { ProecoOgpHead } from '~/components/parts/layout/ProecoOgpHead';
+import { URLS } from '~/constants';
 import { ProecoNextPage } from '~/interfaces/proecoNextPage';
 
 const DashboardTeamPage: ProecoNextPage = () => {
   return (
     <>
       <ProecoOgpHead />
-      TeamPage
+      <Box p={5} mx="auto" maxWidth="1200px">
+        <Link href={URLS.DASHBOARD_TEAMS_NEW}>
+          <Button color="primary" variant="contained">
+            新規チームを作成する
+          </Button>
+        </Link>
+      </Box>
     </>
   );
 };
