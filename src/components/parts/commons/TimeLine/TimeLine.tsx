b7f4d41c626fa98b4b7891f736a77288eef02e03
kanoi-y <furafuratonbo@gmail.com>
2021-10-31 16:00:08 +0900

    🔨 TimeLine.tsxの中身を実装

--- src/components/parts/commons/TimeLine/TimeLine.tsx
+++ src/components/parts/commons/TimeLine/TimeLine.tsx
@@ -1,8 +1,57 @@
 import React, { VFC, ComponentProps } from 'react';
-import { Timeline } from '@mui/lab';
+import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from '@mui/lab';
+import { Typography } from '@mui/material';
+import { UserIcon } from '~/components/domains/user/UserIcon';
 
-type Props = ComponentProps<typeof Timeline>;
+type TimeLineType = {
+  title: string;
+  imagePath: string;
+};
 
-export const TimeLine: VFC<Props> = ({ ...rest }) => {
-  return <Timeline {...rest}></Timeline>;
+type Props = ComponentProps<typeof Timeline> & TimeLineType;
+
+export const TimeLine: VFC<Props> = ({ title, imagePath, children, ...rest }) => {
+  return (
+    <Timeline {...rest}>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot>
+            <UserIcon size="small" imagePath={imagePath} />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Typography variant="h4">{title}</Typography>
+          {children}
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot>
+            <UserIcon size="small" imagePath={imagePath} />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Typography variant="h4">{title}</Typography>
+          {children}
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot>
+            <UserIcon size="small" imagePath={imagePath} />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Typography variant="h4">{title}</Typography>
+          {children}
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
 };
