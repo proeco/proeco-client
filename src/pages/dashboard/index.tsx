a13e48211ef7f59f642aad18ab2d00c040586e3b
itizawa <itizawa.pen@gmail.com>
2021-10-31 17:53:32 +0900

    💄 improve dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,6 @@
+import { Box } from '@mui/system';
 import { ReactNode } from 'react';
+import { Typography } from '~/components/parts/commons';
 import { DashBoardLayout } from '~/components/parts/layout/DashboardLayout';
 import { ProecoOgpHead } from '~/components/parts/layout/ProecoOgpHead';
 import { ProecoNextPage } from '~/interfaces/proecoNextPage';
@@ -7,7 +9,13 @@ const Dashboard: ProecoNextPage = () => {
   return (
     <>
       <ProecoOgpHead />
-      Dashboard
+      <Box p={5} mx="auto" maxWidth="1200px">
+        <Box mb={2} display="flex" alignItems="center" justifyContent="space-between">
+          <Typography variant="h3" bold>
+            ダッシュボード
+          </Typography>
+        </Box>
+      </Box>
     </>
   );
 };
