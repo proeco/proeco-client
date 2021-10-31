649baa7309bdfda2c671078d1b2b9c259f3f2d88
kanoi-y <furafuratonbo@gmail.com>
2021-11-01 00:22:43 +0900

    💄 TimeLineItem.tsxにSpeedDialを追加

--- src/components/parts/commons/TimeLineItem/TimeLineItem.tsx
+++ src/components/parts/commons/TimeLineItem/TimeLineItem.tsx
@@ -1,6 +1,7 @@
 import React, { VFC, ComponentProps } from 'react';
 import { TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from '@mui/lab';
-import { styled } from '@mui/system';
+import { Box, styled } from '@mui/system';
+import { SpeedDial, SpeedDialIcon } from '@mui/material';
 import { UserIcon } from '~/components/domains/user/UserIcon';
 import { Typography } from '~/components/parts/commons';
 
@@ -14,22 +15,40 @@ type Props = ComponentProps<typeof TimelineItem> & TimeLineItemType;
 
 export const TimeLineItem: VFC<Props> = ({ title, imagePath, children, ...rest }) => {
   return (
-    <TimelineItem {...rest}>
-      <TimelineSeparator>
-        <StyledTimeLineDot>
-          <UserIcon size="small" imagePath={imagePath} />
-        </StyledTimeLineDot>
-        <StyledTimeLineConnector />
-      </TimelineSeparator>
-      <TimelineContent>
-        <Typography variant="h4" bold>
-          {title}
-        </Typography>
-        {children}
-      </TimelineContent>
-    </TimelineItem>
+    <StyledDiv>
+      <TimelineItem {...rest}>
+        <TimelineSeparator>
+          <StyledTimeLineDot>
+            <UserIcon size="small" imagePath={imagePath} />
+          </StyledTimeLineDot>
+          <StyledTimeLineConnector />
+        </TimelineSeparator>
+        <StyledTimeLineContent>
+          <Box display="flex" alignItems="center" justifyContent="space-between" width="100%" mb="4px">
+            <Typography variant="h4" bold>
+              {title}
+            </Typography>
+            <StyledSpeedDial ariaLabel="SpeedDial" icon={<SpeedDialIcon />}></StyledSpeedDial>
+          </Box>
+          {children}
+        </StyledTimeLineContent>
+      </TimelineItem>
+    </StyledDiv>
   );
 };
+
+const StyledDiv = styled('div')`
+  .MuiTimelineItem-root {
+    &::before {
+      flex: unset;
+      padding: 0;
+    }
+  }
+`;
+
+const StyledTimeLineContent = styled(TimelineContent)`
+  max-width: fit-content;
+`;
 
 const StyledTimeLineDot = styled(TimelineDot)`
   padding: 0;
@@ -38,4 +57,25 @@ const StyledTimeLineDot = styled(TimelineDot)`
 
 const StyledTimeLineConnector = styled(TimelineConnector)`
   background-color: ${(props) => props.theme.palette.primary.main};
+`;
+
+const StyledSpeedDial = styled(SpeedDial)`
+  .MuiSpeedDial-fab {
+    width: 20px;
+    height: 20px;
+    min-height: unset;
+    padding: 2px;
+  }
+  .MuiSpeedDialIcon-root {
+    height: 100%;
+  }
+  .MuiSpeedDialIcon-icon {
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+  .MuiSpeedDial-actions {
+    margin: 0;
+    padding: 0;
+  }
 `;
