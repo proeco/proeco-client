6ec5dfc5c94d982f82118b88e49d7c02da882a4c
itizawa <itizawa.pen@gmail.com>
2021-10-31 17:51:36 +0900

    ✨ implement create new team ppage

---
+++ src/pages/dashboard/teams/new.tsx
@@ -0,0 +1,26 @@
+import { Box } from '@mui/system';
+import { ReactNode } from 'react';
+import { Typography } from '~/components/parts/commons';
+import { DashBoardLayout } from '~/components/parts/layout/DashboardLayout';
+import { ProecoOgpHead } from '~/components/parts/layout/ProecoOgpHead';
+import { ProecoNextPage } from '~/interfaces/proecoNextPage';
+
+const DashboardTeamPage: ProecoNextPage = () => {
+  return (
+    <>
+      <ProecoOgpHead />
+      <Box p={5} mx="auto" maxWidth="1200px">
+        <Box mb={2} display="flex" alignItems="center" justifyContent="space-between">
+          <Typography variant="h3" bold>
+            新規チームを作成する
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+const getLayout = (page: ReactNode) => <DashBoardLayout>{page}</DashBoardLayout>;
+
+DashboardTeamPage.getLayout = getLayout;
+export default DashboardTeamPage;
