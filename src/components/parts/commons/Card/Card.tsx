43dae46f63db3a7bc9d41ac8d97c3d0d8cbf00f5
itizawa <itizawa.pen@gmail.com>
2021-10-31 18:25:34 +0900

    💄 adjust style

--- src/components/parts/commons/Card/Card.tsx
+++ src/components/parts/commons/Card/Card.tsx
@@ -14,7 +14,6 @@ export const Card: VFC<Props> = ({ padding = 20, square = false, ...rest }) => {
 };
 
 const StyledMuiCard = styled(MuiCard)<{ square: boolean; padding: number }>`
-  width: fit-content;
   padding: ${(props) => props.padding}px;
   border-radius: ${(props) => (props.square ? '0px' : '4px')};
 `;
