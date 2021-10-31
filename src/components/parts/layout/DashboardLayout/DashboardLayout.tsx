60206f05c2ddfb2a62fd826878d1556df51f5076
itizawa <itizawa.pen@gmail.com>
2021-10-31 16:21:24 +0900

    ✨ add sidebar

--- src/components/parts/layout/DashboardLayout/DashboardLayout.tsx
+++ src/components/parts/layout/DashboardLayout/DashboardLayout.tsx
@@ -1,5 +1,13 @@
 import { FC } from 'react';
+import { DashboardModals } from '../DashboardModals';
+import { SideBar } from '../SideBar';
 
 export const DashBoardLayout: FC = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <SideBar />
+      {children}
+      <DashboardModals />
+    </>
+  );
 };
