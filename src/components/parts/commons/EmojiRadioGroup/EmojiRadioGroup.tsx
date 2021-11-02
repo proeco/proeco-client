8c36026fdf830bb801e14436b9284ef4aa8764a2
kanoi-y <furafuratonbo@gmail.com>
2021-11-02 17:19:03 +0900

    💄 見た目の調整

--- src/components/parts/commons/EmojiRadioGroup/EmojiRadioGroup.tsx
+++ src/components/parts/commons/EmojiRadioGroup/EmojiRadioGroup.tsx
@@ -5,22 +5,33 @@ import { Emoji } from '~/components/parts/commons';
 
 type Props = {
   emojiIds: string[];
-  onClick: () => void;
+  onClick: (id: string) => void;
   selectedEmojiId: string;
 };
 
 export const EmojiRadioGroup: VFC<Props> = ({ emojiIds, onClick, selectedEmojiId }) => {
   return (
-    <Box>
-      {emojiIds.map((emojiId) => {
-        <StyledBox selected={selectedEmojiId === emojiId}>
-          <Emoji emojiId={emojiId} size={24} onClick={onClick} />;
-        </StyledBox>;
-      })}
+    <Box display="flex" alignItems="center">
+      {emojiIds.map((emojiId, i) => (
+        <StyledBox key={i} isLast={i === emojiIds.length - 1} selected={selectedEmojiId === emojiId}>
+          <Emoji emojiId={emojiId} size={24} onClick={() => onClick(emojiId)} />
+        </StyledBox>
+      ))}
     </Box>
   );
 };
 
-const StyledBox = styled(Box)<{ selected: boolean }>`
+const StyledBox = styled(Box)<{ isLast: boolean; selected: boolean }>`
+  cursor: pointer;
+  margin-right: ${(props) => (props.isLast ? '0px' : '12px')};
+  padding: 8px;
+  filter: ${(props) => (props.selected ? 'grayscale(0%)' : 'grayscale(100%)')};
+  -webkit-filter: ${(props) => (props.selected ? 'grayscale(0%)' : 'grayscale(100%)')};
   transform: ${(props) => (props.selected ? 'scale(1.35)' : 'scale(1)')};
+  transition: all ease 100ms;
+  &:hover {
+    filter: grayscale(0%);
+    -webkit-filter: grayscale(0%);
+    transform: scale(1.35);
+  }
 `;
