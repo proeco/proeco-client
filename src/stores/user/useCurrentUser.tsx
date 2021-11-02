ef7b40fe806332fc42419fba66b506c9a0285474
itizawa <itizawa.pen@gmail.com>
2021-11-02 13:08:16 +0900

    🚑 fix lint err

--- src/stores/user/useCurrentUser.tsx
+++ src/stores/user/useCurrentUser.tsx
@@ -1,7 +1,6 @@
 import { SWRResponse } from 'swr';
 
 import { restClient } from '~/utils/rest-client';
-import { useSession } from 'next-auth/client';
 import { User } from '~/domains/user';
 import { useAuthenticationSWR } from '~/stores/useAuthenticationSWR';
 
@@ -13,7 +12,7 @@ import { useAuthenticationSWR } from '~/stores/useAuthenticationSWR';
  * @returns mutate データの更新関数
  */
 export const useCurrentUser = (): SWRResponse<User, Error> => {
-return useAuthenticationSWR('/users/me', (endpoint: string) => restClient.apiGet(endpoint).then((result) => result.data), {
+  return useAuthenticationSWR('/users/me', (endpoint: string) => restClient.apiGet(endpoint).then((result) => result.data), {
     revalidateOnFocus: false,
     revalidateOnReconnect: true,
   });
