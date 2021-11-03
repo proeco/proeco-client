208264278d31e7f68af79a143cf036460591cf18
kanoi-y <furafuratonbo@gmail.com>
2021-11-03 18:10:27 +0900

    🔨 menuの中身を実装

--- src/components/domains/team/TeamMenu.stories.tsx
+++ src/components/domains/team/TeamMenu.stories.tsx
@@ -3,18 +3,20 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Box } from '@mui/system';
 
+import { action } from '@storybook/addon-actions';
 import { TeamMenu } from './TeamMenu';
 import { createMockTeam } from '~/mock';
+import { UserIcon } from '~/components/domains/user/UserIcon';
 
 export default {
   title: 'domains/team/TeamMenu',
   component: TeamMenu,
 } as ComponentMeta<typeof TeamMenu>;
 
-const Template: ComponentStory<typeof TeamMenu> = ({ currentTeam }) => {
+const Template: ComponentStory<typeof TeamMenu> = ({ ...rest }) => {
   return (
     <Box width="280px" p="16px">
-      <TeamMenu currentTeam={currentTeam} />
+      <TeamMenu {...rest} />
     </Box>
   );
 };
@@ -24,4 +26,11 @@ const mockTeam = createMockTeam({ name: 'Proeco', iconImage: 'https://itizawa-te
 export const Default = Template.bind({});
 Default.args = {
   currentTeam: mockTeam,
+  menuItems: [
+    {
+      icon: <UserIcon imagePath="https://itizawa-tech.growi.cloud/attachment/616289c6c4e99c0051b30574" size={24} />,
+      text: 'Proeco',
+      onClick: action('clickMenuItem'),
+    },
+  ],
 };
--- src/components/domains/team/TeamMenu.tsx
+++ src/components/domains/team/TeamMenu.tsx
@@ -1,21 +1,39 @@
-import React, { VFC } from 'react';
+import React, { MouseEvent, useState, VFC } from 'react';
 import { Box } from '@mui/system';
 import { UserIcon } from '~/components/domains/user/UserIcon';
-import { IconButton, Typography } from '~/components/parts/commons';
+import { IconButton, Menu, Typography } from '~/components/parts/commons';
 import { Team } from '~/domains';
 
 type Props = {
   currentTeam: Team;
+  menuItems: {
+    icon: JSX.Element;
+    text: string;
+    onClick: () => void;
+  }[];
 };
 
-export const TeamMenu: VFC<Props> = ({ currentTeam }) => {
+export const TeamMenu: VFC<Props> = ({ currentTeam, menuItems }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClickMenu = (event: MouseEvent<HTMLElement>) => {
+    event.stopPropagation();
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box display="flex" alignItems="center" justifyContent="space-between">
       <Box display="flex" alignItems="center" gap="8px">
-        <UserIcon size="small" imagePath={currentTeam.iconImage} />
+        <UserIcon size={40} imagePath={currentTeam.iconImage} />
         <Typography variant="h3">{currentTeam.name}</Typography>
       </Box>
-      <IconButton width={24} icon="KeyboardArrowDown" />
+      <IconButton width={24} icon="KeyboardArrowDown" onClick={(e) => handleClickMenu(e)} />
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose} menuItems={menuItems} />
     </Box>
   );
 };
