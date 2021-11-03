90a9df70c62f68e11145d841e693c9ff6b1ea3c0
kanoi-y <furafuratonbo@gmail.com>
2021-11-03 13:40:58 +0900

    ✨ TeamMenu.tsxを作成

---
+++ src/components/domains/team/TeamMenu.tsx
@@ -0,0 +1,8 @@
+import React, { VFC } from 'react';
+import { Box } from '@mui/system';
+
+type Props = {};
+
+export const TeamMenu: VFC<Props> = ({ ...rest }) => {
+  return <Box {...rest}></Box>;
+};
