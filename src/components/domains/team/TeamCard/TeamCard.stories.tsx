16e598dfaffaea41d2fdd924f3912def1e32754b
kanoi-y <furafuratonbo@gmail.com>
2021-11-06 12:27:05 +0900

    ✨ TeamCard.stories.tsxを作成

---
+++ src/components/domains/team/TeamCard/TeamCard.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Box } from '@mui/system';
+
+import { TeamCard } from './TeamCard';
+import { createMockTeam } from '~/mock';
+
+export default {
+  title: 'domains/team/TeamCard',
+  component: TeamCard,
+} as ComponentMeta<typeof TeamCard>;
+
+const Template: ComponentStory<typeof TeamCard> = ({ ...rest }) => {
+  return (
+    <Box p="40px" bgcolor="gray">
+      <TeamCard {...rest}></TeamCard>
+    </Box>
+  );
+};
+
+const mockTeam = createMockTeam({ name: 'Proeco', iconImage: 'https://itizawa-tech.growi.cloud/attachment/616289c6c4e99c0051b30574' });
+
+export const Default = Template.bind({});
+Default.args = {
+  team: mockTeam,
+};
