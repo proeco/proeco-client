5a123930691b3f56eb335e8e4e84c23230eabe4e
kanoi-y <furafuratonbo@gmail.com>
2021-11-06 14:47:12 +0900

    🎨 loading中やundefinedだった場合に表示が切り替わるように実装

--- src/components/domains/team/TeamCard/TeamCard.stories.tsx
+++ src/components/domains/team/TeamCard/TeamCard.stories.tsx
@@ -20,8 +20,28 @@ const Template: ComponentStory<typeof TeamCard> = ({ ...rest }) => {
 };
 
 const mockTeam = createMockTeam({ name: 'Proeco', iconImage: 'https://itizawa-tech.growi.cloud/attachment/616289c6c4e99c0051b30574' });
+const mockTeamWithoutImage = createMockTeam({ name: 'Proeco' });
 
-export const Default = Template.bind({});
-Default.args = {
+export const DefaultTeamCard = Template.bind({});
+DefaultTeamCard.args = {
   team: mockTeam,
+  isValidating: false,
+};
+
+export const TeamCardWithoutImage = Template.bind({});
+TeamCardWithoutImage.args = {
+  team: mockTeamWithoutImage,
+  isValidating: false,
+};
+
+export const LoadingTeamCard = Template.bind({});
+LoadingTeamCard.args = {
+  team: mockTeam,
+  isValidating: true,
+};
+
+export const UndefinedTeamCard = Template.bind({});
+UndefinedTeamCard.args = {
+  team: undefined,
+  isValidating: false,
 };
--- src/components/domains/team/TeamCard/TeamCard.tsx
+++ src/components/domains/team/TeamCard/TeamCard.tsx
@@ -1,28 +1,68 @@
-import React, { VFC } from 'react';
-import { Card as MuiCard } from '@mui/material';
+import React, { useMemo, VFC } from 'react';
+import { Avatar, Card as MuiCard, Skeleton } from '@mui/material';
 import { Box, styled } from '@mui/system';
 import { Team } from '~/domains';
 import { UserIcon } from '~/components/domains/user/UserIcon';
-import { Link, Typography } from '~/components/parts/commons';
+import { Link, Typography, Icon } from '~/components/parts/commons';
 
 type Props = {
-  team: Team;
+  team?: Team;
+  isValidating: boolean;
 };
 
-export const TeamCard: VFC<Props> = ({ team }) => {
+export const TeamCard: VFC<Props> = ({ team, isValidating }) => {
+  const IconContent = useMemo(() => {
+    if (isValidating) return <Skeleton variant="circular" width={40} height={40} />;
+
+    if (team) {
+      if (team.iconImage) return <UserIcon size={40} imagePath={team.iconImage} />;
+      //TODO Avatarをcomponent化する
+      return <Avatar>{team.name[0]}</Avatar>;
+    }
+
+    return <Icon width={40} icon="Group" />;
+  }, [isValidating, team]);
+
+  const NameContent = useMemo(() => {
+    if (isValidating) return <Skeleton variant="text" width="100px" />;
+
+    if (team) {
+      return (
+        <Typography variant="h3" noWrap width="220px">
+          {team.name}
+        </Typography>
+      );
+    }
+
+    return <Typography variant="h3">undefined</Typography>;
+  }, [isValidating, team]);
+
+  const DescriptionContent = useMemo(() => {
+    if (isValidating) {
+      return (
+        <>
+          <Skeleton variant="text" width="260px" />
+          <Skeleton variant="text" width="260px" />
+        </>
+      );
+    }
+
+    if (team) {
+      return <StyledDescription variant="caption">{team.description}</StyledDescription>;
+    }
+
+    return <Typography variant="caption">undefined</Typography>;
+  }, [isValidating, team]);
+
   return (
     <StyledBox>
-      <Link href={`/teams/${team._id}`}>
+      <Link href={`/teams/${team?._id}`}>
         <StyledTeamCard>
           <Box display="flex" alignItems="center" mb="8px">
-            <Box mr="8px">
-              <UserIcon imagePath={team.iconImage} size={40} />
-            </Box>
-            <Typography variant="h3">{team.name}</Typography>
+            <Box mr="8px">{IconContent}</Box>
+            {NameContent}
           </Box>
-          <Typography variant="caption" lineHeight="1.5" letterSpacing="0" display="block">
-            {team.description}
-          </Typography>
+          {DescriptionContent}
         </StyledTeamCard>
       </Link>
     </StyledBox>
@@ -48,4 +88,13 @@ const StyledTeamCard = styled(MuiCard)`
   height: 130px;
   padding: 20px;
   box-sizing: border-box;
+`;
+
+const StyledDescription = styled(Typography)`
+  line-height: 1.5;
+  letter-spacing: 0;
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 `;
