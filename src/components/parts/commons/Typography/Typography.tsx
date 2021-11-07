27f57955c64430b1f78d9531f61025402e3d441d
kanoi-y <furafuratonbo@gmail.com>
2021-11-07 18:37:13 +0900

    🔨 Typography.tsxにMaximumLinesを追加

--- src/components/parts/commons/Typography/Typography.tsx
+++ src/components/parts/commons/Typography/Typography.tsx
@@ -4,15 +4,20 @@ import { styled } from '@mui/material/styles';
 
 type Bold = {
   bold?: boolean;
+  MaximumLines?: number;
 };
 
 type Props = ComponentProps<typeof MuiTypography> & Bold;
 
-export const Typography: VFC<Props> = ({ bold, color = 'textColor.main', ...rest }) => {
-  return <StyledTypography color={color} bold={bold ? 1 : 0} {...rest} />;
+export const Typography: VFC<Props> = ({ bold, MaximumLines = 1, color = 'textColor.main', ...rest }) => {
+  return <StyledTypography color={color} bold={bold ? 1 : 0} MaximumLines={MaximumLines} {...rest} />;
 };
 
-const StyledTypography = styled(MuiTypography)<{ bold: number }>`
+const StyledTypography = styled(MuiTypography)<{ bold: number; MaximumLines: number }>`
   text-transform: none;
   font-weight: ${(props) => (props.bold ? 'bold' : 'normal')};
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-line-clamp: ${(props) => props.MaximumLines};
+  -webkit-box-orient: vertical;
 `;
