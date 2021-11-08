de0d70035f505a12d76b1d64011350be7349b9cf
kanoi-y <furafuratonbo@gmail.com>
2021-11-08 19:38:18 +0900

    🔨 teamのIconとteam名を表示

--- src/components/domains/team/TeamSideBar/TeamSideBar.tsx
+++ src/components/domains/team/TeamSideBar/TeamSideBar.tsx
@@ -1,18 +1,23 @@
-import { ComponentProps, memo, VFC } from 'react';
+import { ComponentProps, memo, useMemo, VFC } from 'react';
 import { useRouter } from 'next/router';
 
 import { Box, styled } from '@mui/system';
-import { TeamMenu } from '../TeamMenu';
 
+import { Skeleton } from '@mui/material';
 import { Typography, SideBarListItem, Icon, Link } from '~/components/parts/commons';
+import { TeamIcon } from '~/components/domains/team/TeamIcon';
 
-import { User } from '~/domains';
+import { Team, User } from '~/domains';
 
 import { URLS } from '~/constants/urls';
+import { useCurrentUser } from '~/stores/user/useCurrentUser';
+import { useTeams } from '~/stores/team';
 
 type Props = {
   currentUser?: User;
+  currentTeam?: Team;
   asPath: string;
+  isValidating: boolean;
 };
 
 const sidebarItems: {
@@ -37,10 +42,39 @@ const sidebarItems: {
   },
 ];
 
-export const Component: VFC<Props> = memo(({ asPath }) => {
+export const Component: VFC<Props> = memo(({ asPath, currentTeam, isValidating }) => {
+  const TeamContent = useMemo(() => {
+    if (isValidating) {
+      return (
+        <Box>
+          <Skeleton variant="circular" width={80} height={80} />
+          <Skeleton variant="text" width="100px" />
+        </Box>
+      );
+    }
+
+    if (currentTeam) {
+      return (
+        <Box>
+          <TeamIcon team={currentTeam} size={80} />
+          <Typography variant="h3" maximum_lines={1}>
+            {currentTeam.name}
+          </Typography>
+        </Box>
+      );
+    }
+
+    return (
+      <Box>
+        <Icon width={40} icon="Group" />
+        <Typography variant="h3">undefined</Typography>
+      </Box>
+    );
+  }, [isValidating, currentTeam]);
+
   return (
     <StyledSideBarWrapper width="280px" minHeight="100vh" p="16px" bgcolor="whitesmoke">
-      <TeamMenu />
+      {TeamContent}
       <Box p="12px 0 24px" display="flex" flexDirection="column" gap="8px">
         {sidebarItems.map((sidebarItem, index) => {
           return (
@@ -66,6 +100,12 @@ const StyledSideBarWrapper = styled(Box)`
 
 export const TeamSideBar: VFC = memo(() => {
   const router = useRouter();
+  const { data: currentUser } = useCurrentUser();
+  const { data: teams, isValidating: isValidatingTeams } = useTeams({
+    userId: currentUser?._id,
+  });
 
-  return <Component asPath={router.asPath} />;
+  const currentTeam = teams?.find((team) => team._id === router.query.id);
+
+  return <Component asPath={router.asPath} currentTeam={currentTeam} isValidating={isValidatingTeams} />;
 });
