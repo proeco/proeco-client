74dead3ba9c780a7fbe293bfa65c9ecae2fb610b
kanoi-y <furafuratonbo@gmail.com>
2021-11-08 17:13:00 +0900

    🎨 FirstLetterIconに置き換え

--- src/components/domains/team/TeamIcon/TeamIcon.tsx
+++ src/components/domains/team/TeamIcon/TeamIcon.tsx
@@ -1,7 +1,6 @@
-import { Avatar } from '@mui/material';
-import { styled } from '@mui/system';
 import React, { VFC } from 'react';
 import { UserIcon } from '~/components/domains/user/UserIcon';
+import { FirstLetterIcon } from '~/components/parts/commons/FirstLetterIcon';
 import { Team } from '~/domains';
 
 type Props = {
@@ -11,12 +10,5 @@ type Props = {
 
 export const TeamIcon: VFC<Props> = ({ team, size = 40 }) => {
   if (team.iconImage) return <UserIcon size={size} imagePath={team.iconImage} />;
-  /* TODO Avatarをcomponent化する */
-  return <StyledAvatar size={size}>{team.name[0]}</StyledAvatar>;
+  return <FirstLetterIcon size={size} name={team.name} />;
 };
-
-const StyledAvatar = styled(Avatar)<{ size: number }>`
-  width: ${(props) => props.size}px;
-  height: ${(props) => props.size}px;
-  font-size: ${(props) => (props.size / 40) * 1.25}rem;
-`;
