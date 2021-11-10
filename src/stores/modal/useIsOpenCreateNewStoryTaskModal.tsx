c51bb97a6d4ed9ec01c4920caf2bb87f5338f65c
kanoi-y <furafuratonbo@gmail.com>
2021-11-10 22:29:35 +0900

    ✨ useIsOpenCreateNewStoryTaskModalを作成

---
+++ src/stores/modal/useIsOpenCreateNewStoryTaskModal.tsx
@@ -0,0 +1,13 @@
+import { SWRResponse } from 'swr';
+import { useStaticSWR } from '~/stores/useStaticSWR';
+
+/**
+ * モーダルの開閉状況を取得するSWR
+ * @returns data モーダルの開閉状態 boolean
+ * @returns isValidating 取得中を表す boolean
+ * @returns error エラー
+ * @returns mutate データの更新関数
+ */
+export const useIsOpenCreateNewStoryTaskModal = (initialData?: boolean): SWRResponse<boolean, Error> => {
+  return useStaticSWR<boolean, Error>('useIsOpenCreateNewStoryTaskModal', initialData);
+};
