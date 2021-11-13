53e5a6494b7008a4c549be7f7a8dcf2424622557
kanoi-y <furafuratonbo@gmail.com>
2021-11-13 20:58:32 +0900

    💄 ストーリー詳細画面にstoryTaskを表示

--- src/pages/team/[teamId]/dashboard/story/[storyId].tsx
+++ src/pages/team/[teamId]/dashboard/story/[storyId].tsx
@@ -1,4 +1,4 @@
-import React, { useState, MouseEvent, ReactNode } from 'react';
+import React, { useState, MouseEvent, ReactNode, ComponentProps } from 'react';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 
@@ -11,7 +11,7 @@ import { useIsOpenUpdateStoryModal } from '~/stores/modal/useIsOpenUpdateStoryMo
 import { useIsOpenDeleteStoryModal } from '~/stores/modal/useIsOpenDeleteStoryModal';
 import { useStoryForUpdate, useStoryForDelete } from '~/stores/story';
 
-import { Button, Icon, Typography } from '~/components/parts/commons';
+import { Button, Icon, TimeLine, Typography } from '~/components/parts/commons';
 import { ProecoOgpHead } from '~/components/parts/layout/ProecoOgpHead';
 import { useStory } from '~/stores/story/useStory';
 import { Menu } from '~/components/parts/commons/Menu';
@@ -20,6 +20,8 @@ import { ProecoNextPage } from '~/interfaces/proecoNextPage';
 import { COLORS } from '~/constants';
 import { TeamDashboardLayout } from '~/components/parts/layout/TeamDashboardLayout';
 import { useIsOpenCreateNewStoryTaskModal } from '~/stores/modal/useIsOpenCreateNewStoryTaskModal';
+import { useStoryTasks } from '~/stores/storyTask';
+import { useTeam } from '~/stores/team';
 
 type Props = {
   storyFromServerSide?: Story;
@@ -28,8 +30,49 @@ type Props = {
 const StoryPage: ProecoNextPage<Props> = ({ storyFromServerSide }) => {
   const router = useRouter();
 
-  const { storyId } = router.query;
+  const { teamId, storyId } = router.query;
   const { data: story } = useStory(storyId as string, storyFromServerSide);
+  const { data: currentTeam } = useTeam({
+    teamId: teamId as string,
+  });
+  const page = router.query.page ? Number(router.query.page) : 1;
+
+  const { data: storyTasks } = useStoryTasks({
+    storyId: storyId as string,
+    page: page,
+    limit: 10,
+  });
+
+  const timeLineItems: {
+    title: string;
+    imagePath?: string;
+    children: ReactNode;
+    actions: {
+      icon: ComponentProps<typeof Icon>['icon'];
+      name: string;
+      onClick: () => void;
+    }[];
+  }[] = storyTasks
+    ? storyTasks.docs.map((storyTask) => {
+        return {
+          title: storyTask.title,
+          imagePath: currentTeam?.iconImage,
+          children: <Box width="500px" height="250px"></Box>,
+          actions: [
+            {
+              icon: 'Delete',
+              name: '削除',
+              onClick: () => console.log('削除'),
+            },
+            {
+              icon: 'Update',
+              name: '更新',
+              onClick: () => console.log('更新'),
+            },
+          ],
+        };
+      })
+    : [];
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -100,6 +143,7 @@ const StoryPage: ProecoNextPage<Props> = ({ storyFromServerSide }) => {
         <Button variant="contained" onClick={handleClickCreateStoryTaskButton}>
           タスクを作成する
         </Button>
+        <Box>{<TimeLine timeLineItems={timeLineItems} />}</Box>
       </Box>
     </>
   );
