438d99625cecad94d8f41c0ab85dac244f9db78a
itizawa <itizawa.pen@gmail.com>
2021-11-14 11:24:43 +0900

    ✨ implement init

---
+++ .storybook/storyshots.spec.ts
@@ -0,0 +1,7 @@
+import initStoryshots, { multiSnapshotWithOptions } from "@storybook/addon-storyshots";
+
+jest.mock("global", () => Object.assign(global, { window: { STORYBOOK_HOOKS_CONTEXT: "" } }));
+
+initStoryshots({
+  test: multiSnapshotWithOptions(),
+});
