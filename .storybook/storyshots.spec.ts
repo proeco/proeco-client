420795c55374ec7c47d0b3ce2059162416c819f5
itizawa <itizawa.pen@gmail.com>
2021-11-14 11:29:27 +0900

    🚚 move

--- .storybook/storyshots.spec.ts
+++
@@ -1,7 +0,0 @@
-import initStoryshots, { multiSnapshotWithOptions } from "@storybook/addon-storyshots";
-
-jest.mock("global", () => Object.assign(global, { window: { STORYBOOK_HOOKS_CONTEXT: "" } }));
-
-initStoryshots({
-  test: multiSnapshotWithOptions(),
-});
