d6ebeea67a55ffc579be7fc4cc5be70ee2912413
itizawa <itizawa.pen@gmail.com>
2021-11-14 11:58:01 +0900

    ✨ refactor extension

--- src/storyshots.spec.ts
+++ src/storyshots.spec.ts
@@ -1,7 +1,11 @@
-import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots';
+import initStoryshots, { multiSnapshotWithOptions, Stories2SnapsConverter } from '@storybook/addon-storyshots';
 
 jest.mock('global', () => Object.assign(global, { window: { STORYBOOK_HOOKS_CONTEXT: '', document: { createElement: () => void 0 } } }));
 
 initStoryshots({
   test: multiSnapshotWithOptions(),
+  stories2snapsConverter: new Stories2SnapsConverter({
+    snapshotExtension: '.tsx.snap',
+    storiesExtensions: ['.tsx'],
+  }),
 });
