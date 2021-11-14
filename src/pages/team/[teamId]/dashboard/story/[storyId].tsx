434a89c6430cb10471b0d3980988978e88404952
itizawa <itizawa.pen@gmail.com>
2021-11-15 01:52:46 +0900

    💄 display paper

--- src/pages/team/[teamId]/dashboard/story/[storyId].tsx
+++ src/pages/team/[teamId]/dashboard/story/[storyId].tsx
@@ -11,7 +11,7 @@ import { useIsOpenUpdateStoryModal } from '~/stores/modal/useIsOpenUpdateStoryMo
 import { useIsOpenDeleteStoryModal } from '~/stores/modal/useIsOpenDeleteStoryModal';
 import { useStoryForUpdate, useStoryForDelete } from '~/stores/story';
 
-import { Button, Emoji, Icon, TimeLine, Typography } from '~/components/parts/commons';
+import { Button, Emoji, Icon, Paper, TimeLine, Typography } from '~/components/parts/commons';
 import { ProecoOgpHead } from '~/components/parts/layout/ProecoOgpHead';
 import { useStory } from '~/stores/story/useStory';
 import { Menu } from '~/components/parts/commons/Menu';
@@ -132,7 +132,7 @@ const StoryPage: ProecoNextPage<Props> = ({ storyFromServerSide }) => {
     <>
       <ProecoOgpHead title={story.title} description={story.description} />
       <Box p={5} mx="auto" maxWidth="1200px">
-        <Box mb={2} display="flex" alignItems="center" justifyContent="space-between">
+        <Box mb={4} display="flex" alignItems="center" justifyContent="space-between">
           <Box display="flex" alignItems="center" gap="16px">
             <Emoji emojiId={story.emojiId} size={40} />
             <Typography variant="h2" bold>
@@ -142,7 +142,9 @@ const StoryPage: ProecoNextPage<Props> = ({ storyFromServerSide }) => {
           <IconButton icon="MoreVert" width={24} onClick={(e) => handleClickMenu(e)} />
           <Menu onClick={(e) => e.stopPropagation()} anchorEl={anchorEl} open={open} menuItems={menuItems} onClose={handleClose} />
         </Box>
-        <Typography variant="h4">{story.description}</Typography>
+        <Paper>
+          <Typography variant="h4">{story.description}</Typography>
+        </Paper>
         <TimeLine timeLineItems={timeLineItems} />
         <Button variant="contained" onClick={handleClickCreateStoryTaskButton}>
           タスクを作成する
