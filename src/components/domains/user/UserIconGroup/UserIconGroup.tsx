e9313419695e48714a8ccce1e18d6044c23f0893
itizawa <itizawa.pen@gmail.com>
2021-11-14 12:55:28 +0900

    💄 adjust style

--- src/components/domains/user/UserIconGroup/UserIconGroup.tsx
+++ src/components/domains/user/UserIconGroup/UserIconGroup.tsx
@@ -1,4 +1,5 @@
 import { VFC } from 'react';
+import { styled } from '@mui/material/styles';
 
 import { AvatarGroup } from '@mui/material';
 import { User } from '~/domains';
@@ -13,10 +14,16 @@ type Props = {
 
 export const UserIconGroup: VFC<Props> = ({ users, maxCount = 3, isLink = false }) => {
   return (
-    <AvatarGroup max={maxCount}>
+    <StyledAvatarGroup max={maxCount}>
       {users.map((user) => {
         return <UserIcon key={user._id} size={48} userId={user._id} imagePath={user.image} isLink={isLink} />;
       })}
-    </AvatarGroup>
+    </StyledAvatarGroup>
   );
 };
+
+const StyledAvatarGroup = styled(AvatarGroup)`
+  /* デフォルトで右寄せになっているアイコンを左寄せにするため */
+  display: flex;
+  justify-content: start;
+`;
