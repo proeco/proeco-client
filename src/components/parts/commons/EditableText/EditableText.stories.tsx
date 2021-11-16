43e2dc680f8a97788841a5f404c476dc64ca3b44
kanoi-y <furafuratonbo@gmail.com>
2021-11-16 17:12:59 +0900

    ✨ EditableText.stories.tsxを作成

---
+++ src/components/parts/commons/EditableText/EditableText.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Box } from '@mui/system';
+import { EditableText } from './EditableText';
+
+export default {
+  title: 'parts/commons/EditableText',
+  component: EditableText,
+  argTypes: { onChange: { action: 'onChange' } },
+} as ComponentMeta<typeof EditableText>;
+
+const Template: ComponentStory<typeof EditableText> = ({ ...rest }) => {
+  return (
+    <Box p="20px" bgcolor="#e5e5e5">
+      <EditableText {...rest}></EditableText>
+    </Box>
+  );
+};
+
+export const DefaultEditableText = Template.bind({});
+DefaultEditableText.args = {
+  variant: 'body1',
+  value: 'EditableText',
+};
+
+export const multilineEditableText = Template.bind({});
+multilineEditableText.args = {
+  ...DefaultEditableText.args,
+  value: 'これは、複数行のEditableTextです',
+  multiline: true,
+};
+
+export const ErrorEditableText = Template.bind({});
+ErrorEditableText.args = {
+  ...DefaultEditableText.args,
+  error: true,
+};
