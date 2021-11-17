cd6c7a3d7fb96ec35ae0290402dfd72f91e4eaa5
itizawa <itizawa.pen@gmail.com>
2021-11-18 00:48:33 +0900

    ✨ set access token

--- src/hooks/useAuth/useAuth.ts
+++ src/hooks/useAuth/useAuth.ts
@@ -1,14 +1,20 @@
-import { signInWithPopup } from '@firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
+import { setCookie } from 'nookies';
 import { auth, googleAuthProvider } from '~/libs/firebase';
 
 export const useAuth = () => {
   const login = () => {
     signInWithPopup(auth, googleAuthProvider)
       .then((result) => {
-        console.log(result);
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (!credential?.accessToken) {
+          return;
+        }
 
-        // setCredential(result);
-        // setState('logined');
+        setCookie(null, 'access-token', credential.accessToken, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
+        });
       })
       .catch((e) => {
         console.log(e);
