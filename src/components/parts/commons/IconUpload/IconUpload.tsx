b2c4b025c9fdba945350c3dfc4d6c8515abae114
kanoi-y <furafuratonbo@gmail.com>
2021-11-18 16:12:09 +0900

    🔨 アイコンをクリックで画像選択できるように実装

--- src/components/parts/commons/IconUpload/IconUpload.tsx
+++ src/components/parts/commons/IconUpload/IconUpload.tsx
@@ -1,17 +1,43 @@
 import React, { VFC, ChangeEvent } from 'react';
-import { Box } from '@mui/system';
+import { styled } from '@mui/system';
+import { Avatar } from '@mui/material';
+import { Icon } from '../Icon/Icon';
 import { UserIcon } from '~/components/domains/user/UserIcon';
 
 type Props = {
   onSelectImage: (file: ChangeEvent<HTMLInputElement>) => void;
-  currentImage: string;
+  currentImage?: string;
+  size?: number;
 };
 
-export const IconUpload: VFC<Props> = ({ onSelectImage, currentImage }) => {
+export const IconUpload: VFC<Props> = ({ onSelectImage, currentImage, size = 40 }) => {
   return (
-    <Box>
-      <UserIcon size={40} imagePath={currentImage} />
-      <input type="file" name="image" onChange={onSelectImage} accept="image/*" />
-    </Box>
+    <label htmlFor="image">
+      {currentImage ? (
+        <StyledUserIcon size={size} imagePath={currentImage} />
+      ) : (
+        <StyledAvatar size={size}>
+          <Icon icon="Group" color="#ccc" width="100%" />
+        </StyledAvatar>
+      )}
+      <StyledInput type="file" name="image" id="image" onChange={onSelectImage} accept="image/*" />
+    </label>
   );
 };
+
+const StyledUserIcon = styled(UserIcon)`
+  cursor: pointer;
+`;
+
+const StyledAvatar = styled(Avatar)<{ size: number }>`
+  background-color: white;
+  border: 2px solid ${(props) => props.theme.palette.primary.main};
+  box-sizing: border-box;
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+  cursor: pointer;
+`;
+
+const StyledInput = styled('input')`
+  display: none;
+`;
