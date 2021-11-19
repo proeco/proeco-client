dca816628599e0b3553c45a3a8096474d9acea51
kanoi-y <furafuratonbo@gmail.com>
2021-11-19 12:31:52 +0900

    ✨ TeamSIdeBar.stories.tsxを作成

---
+++ src/components/domains/team/TeamSideBar/TeamSideBar.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { action } from '@storybook/addon-actions';
+import { Component } from './TeamSideBar';
+
+import { createMockTeam, createMockUser } from '~/mock';
+
+import { URLS } from '~/constants/urls';
+import { Icon } from '~/components/parts/commons';
+
+export default {
+  title: 'domains/team/TeamSideBar',
+  component: Component,
+} as ComponentMeta<typeof Component>;
+
+const Template: ComponentStory<typeof Component> = ({ ...rest }) => {
+  return <Component {...rest} />;
+};
+
+const mockTeam = createMockTeam({ name: 'Proeco', iconImage: 'https://itizawa-tech.growi.cloud/attachment/616289c6c4e99c0051b30574' });
+const mockUser = createMockUser({ image: 'https://storage.cloud.google.com/proeco/6190af4da62f638c34cdbcbf/team-icons/myicon.png' });
+
+export const Default = Template.bind({});
+Default.args = {
+  currentTeam: mockTeam,
+  asPath: URLS.DASHBOARD,
+  isValidating: false,
+  currentUser: mockUser,
+  teamUsers: [mockUser],
+  menuItems: [
+    {
+      icon: <Icon icon="Logout" width="20px" color="textColor.main" />,
+      text: 'Logout',
+      onClick: action('signOut'),
+    },
+  ],
+};
