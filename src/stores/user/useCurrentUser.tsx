e4a75a30834efff5acb5e720d528e7937ee38b9d
itizawa <itizawa.pen@gmail.com>
2021-11-20 20:42:36 +0900

    ✨ refactor useCurrentUser

--- src/stores/user/useCurrentUser.tsx
+++ src/stores/user/useCurrentUser.tsx
@@ -1,30 +1,18 @@
-// import { SWRResponse } from 'swr';
+import useSWR, { SWRResponse } from 'swr';
 
-// import { restClient } from '~/utils/rest-client';
-// import { User } from '~/domains/user';
-// import { useAuthenticationSWR } from '~/stores/useAuthenticationSWR';
+import { restClient } from '~/utils/rest-client';
+import { User } from '~/domains/user';
 
-// /**
-//  * 現在ログイン中のユーザーを取得するSWR
-//  * @returns data ログイン中のユーザー
-//  * @returns isValidating 取得中を表す boolean
-//  * @returns error エラー
-//  * @returns mutate データの更新関数
-//  */
-// export const useCurrentUser = (): SWRResponse<User, Error> => {
-//   return useAuthenticationSWR('/users/me', (endpoint: string) => restClient.apiGet(endpoint).then((result) => result.data), {
-//     revalidateOnFocus: false,
-//     revalidateOnReconnect: true,
-//   });
-// };
-
-import { useContext } from 'react';
-import { CurrentUserContext } from '~/contexts/CurrentUserProvider';
-
-export const useCurrentUser = (): {
-  currentUser: any;
-} => {
-  const { currentUser } = useContext(CurrentUserContext);
-
-  return { currentUser };
+/**
+ * 現在ログイン中のユーザーを取得するSWR
+ * @returns data ログイン中のユーザー
+ * @returns isValidating 取得中を表す boolean
+ * @returns error エラー
+ * @returns mutate データの更新関数
+ */
+export const useCurrentUser = (): SWRResponse<User, Error> => {
+  return useSWR('/users/me', (endpoint: string) => restClient.apiGet(endpoint).then((result) => result.data), {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: true,
+  });
 };
