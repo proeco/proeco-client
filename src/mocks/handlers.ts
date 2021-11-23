fc0fa3323aa0692974db89a0e17792a8dcd5d84c
itizawa <itizawa.pen@gmail.com>
2021-11-24 01:13:40 +0900

    ✨ setup

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,4 +2,7 @@ import { rest } from 'msw';
 import { getAttachmentById } from './api/attachment';
 import { getCurrentUser } from './api/user';
 
-export const handlers = [rest.get('/api/v1/users/me', getCurrentUser), rest.get('/api/v1/attachments/:id', getAttachmentById)];
+export const handlers = [
+  rest.get('http://localhost:8000/api/v1/users/me', getCurrentUser),
+  rest.get('http://localhost:8000/api/v1/attachments/:id', getAttachmentById),
+];
