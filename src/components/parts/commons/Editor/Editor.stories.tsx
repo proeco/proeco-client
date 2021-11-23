c96b9e4e83e00aa86bbf7b7055b93b6efcae4193
kanoi-y <furafuratonbo@gmail.com>
2021-11-24 06:13:51 +0900

    ✨ Editor.stories.tsxを作成

---
+++ src/components/parts/commons/Editor/Editor.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Box } from '@mui/system';
+
+import { Editor } from './Editor';
+
+export default {
+  title: 'parts/commons/Editor',
+  component: Editor,
+} as ComponentMeta<typeof Editor>;
+
+const Template: ComponentStory<typeof Editor> = ({ ...rest }) => {
+  return (
+    <Box p="20px" width="600px" bgcolor="#e5e5e5">
+      <Editor {...rest} />
+    </Box>
+  );
+};
+
+export const Default = Template.bind({});
--- src/components/parts/commons/Editor/Editor.tsx
+++ src/components/parts/commons/Editor/Editor.tsx
@@ -1,8 +1,26 @@
 import React, { VFC } from 'react';
-import { Box } from '@mui/system';
+import { Box, styled } from '@mui/system';
+import { Tab, Tabs } from '@mui/material';
+
+import { TextField, Button } from '~/components/parts/commons';
 
 type Props = {};
 
 export const Editor: VFC<Props> = ({ ...rest }) => {
-  return <Box {...rest}></Box>;
+  return (
+    <Box bgcolor="white" p="16px" {...rest}>
+      <Box>
+        <Tabs aria-label="Editor tabs">
+          <Tab label="Markdown" />
+          <Tab label="Preview" />
+        </Tabs>
+        <TextField />
+      </Box>
+      <Button variant="contained">投稿する</Button>
+    </Box>
+  );
 };
+
+// const StyledTextField = styled(TextField)`
+//   width: 100%;
+// `;
