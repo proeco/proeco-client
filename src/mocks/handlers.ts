fab7363cf2872e2f9af387452968e2529d531b91
itizawa <itizawa.pen@gmail.com>
2021-11-24 01:42:00 +0900

    ✨ implement generateRoute

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,8 @@ import { rest } from 'msw';
 import { getAttachmentById } from './api/attachment';
 import { getCurrentUser } from './api/user';
 
-export const handlers = [
-  rest.get('http://localhost:8000/api/v1/users/me', getCurrentUser),
-  rest.get('http://localhost:8000/api/v1/attachments/:id', getAttachmentById),
-];
+const generateRoute = (path: string) => {
+  return `${process.env.NEXT_PUBLIC_BACKEND_URL_FROM_CLIENT}/api/v1/${path}`;
+};
+
+export const handlers = [rest.get(generateRoute('/users/me'), getCurrentUser), rest.get(generateRoute('/attachments/:id'), getAttachmentById)];
