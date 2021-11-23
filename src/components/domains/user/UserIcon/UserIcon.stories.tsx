43993d852b17a517b2e2e4d628bf48478bd765dc
itizawa <itizawa.pen@gmail.com>
2021-11-24 01:43:31 +0900

    🔥 remove

--- src/components/domains/user/UserIcon/UserIcon.stories.tsx
+++ src/components/domains/user/UserIcon/UserIcon.stories.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Box } from '@mui/system';
 
-import { rest } from 'msw';
 import { UserIcon, GuestUserIcon } from './UserIcon';
 import { createMockUser } from '~/mocks/domains/createMockUser';
-import { worker } from '~/mocks/browser';
 
 export default {
   title: 'domains/user/UserIcon',
@@ -13,11 +11,6 @@ export default {
 } as ComponentMeta<typeof UserIcon>;
 
 const LoginUser: ComponentStory<typeof UserIcon> = ({ ...props }) => {
-  worker.use(
-    rest.get('http://localhost:8000/api/v1/attachments/:id', (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json('https://itizawa-tech.growi.cloud/attachment/619cf4648062e8c5ac7e684f'));
-    }),
-  );
   return (
     <Box width="300px" bgcolor="gray" p={4} display="flex" alignItems="center" justifyContent="space-around">
       <UserIcon {...props} size={40} />
