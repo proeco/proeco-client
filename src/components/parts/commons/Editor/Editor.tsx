638a0c07b6d3adbb5d273b22b7c6c72f732bac0a
kanoi-y <furafuratonbo@gmail.com>
2021-11-24 23:01:24 +0900

    ✨ TabPanelを作成

--- src/components/parts/commons/Editor/Editor.tsx
+++ src/components/parts/commons/Editor/Editor.tsx
@@ -1,9 +1,29 @@
 import React, { VFC, useState } from 'react';
-import { Box, styled } from '@mui/system';
-// import { styled } from '@mui/material/styles';
+import { Box } from '@mui/system';
 import { Tab, Tabs } from '@mui/material';
 
 import { TextField, Button } from '~/components/parts/commons';
+
+type TabPanelProps = {
+  textValue: string;
+  index: number;
+  value: number;
+};
+
+const TabPanel: VFC<TabPanelProps> = ({ textValue, index, value }) => {
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`}>
+      {value === index && <TextField multiline fullWidth value={textValue} />}
+    </div>
+  );
+};
+
+const a11yProps = (index: number) => {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+};
 
 type Props = {};
 
@@ -18,16 +38,13 @@ export const Editor: VFC<Props> = ({ ...rest }) => {
     <Box bgcolor="white" p="16px" {...rest}>
       <Box>
         <Tabs value={value} onChange={handleChange} aria-label="Editor tabs">
-          <Tab label="Markdown" />
-          <Tab label="Preview" />
+          <Tab label="Markdown" {...a11yProps(0)} />
+          <Tab label="Preview" {...a11yProps(1)} />
         </Tabs>
-        <StyledTextField multiline />
+        <TabPanel value={value} index={0} textValue="Markdown" />
+        <TabPanel value={value} index={1} textValue="Preview" />
       </Box>
       <Button variant="contained">投稿する</Button>
     </Box>
   );
 };
-
-const StyledTextField = styled(TextField)`
-  width: 100%;
-`;
