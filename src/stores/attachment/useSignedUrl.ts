720368552067709452b2abb9119a72aea4d27fad
itizawa <itizawa.pen@gmail.com>
2021-11-25 23:22:42 +0900

    🚚 rename

--- src/stores/attachment/useSignedUrl.ts
+++ src/stores/attachment/useSignedUrl.ts
@@ -5,14 +5,14 @@ import { restClient } from '~/utils/rest-client';
 import { Attachment } from '~/domains';
 
 /**
- * attachmentUrlを取得するSWR
+ * signedUrlを取得するSWR
  * @params attachmentId 取得対象のファイルID
  * @returns data attachmentUrl
  * @returns isValidating 取得中を表す boolean
  * @returns error エラー
  * @returns mutate データの更新関数
  */
-export const useAttachmentUrl = (attachmentId?: Attachment['_id']): SWRResponse<string, Error> => {
+export const useSignedUrl = (attachmentId?: Attachment['_id']): SWRResponse<string, Error> => {
   const key = attachmentId ? `/attachments/${attachmentId}` : null;
 
   return useSWRImmutable(key, (endpoint: string) => restClient.apiGet(endpoint).then((result) => result.data));
