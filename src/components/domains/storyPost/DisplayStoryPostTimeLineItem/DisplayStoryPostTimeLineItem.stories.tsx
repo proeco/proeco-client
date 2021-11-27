a81af63a565cefb1910d51f745ae001fb8dff2d6
kanoi-y <furafuratonbo@gmail.com>
2021-11-27 21:22:06 +0900

    ✨ DisplayStoryPostTimeLineItem.stories.tsxを作成

---
+++ src/components/domains/storyPost/DisplayStoryPostTimeLineItem/DisplayStoryPostTimeLineItem.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Box } from '@mui/system';
+import { DisplayStoryPostTimeLineItem } from './DisplayStoryPostTimeLineItem';
+import { createMockStoryPost, createMockUser } from '~/mocks/domains';
+import { Paper } from '~/components/parts/commons';
+
+export default {
+  title: 'domains/storyPost/DisplayStoryPostTimeLineItem',
+  component: DisplayStoryPostTimeLineItem,
+} as ComponentMeta<typeof DisplayStoryPostTimeLineItem>;
+
+const Template: ComponentStory<typeof DisplayStoryPostTimeLineItem> = ({ ...rest }) => {
+  return (
+    <Box p="20px" width="600px" bgcolor="#e5e5e5">
+      <Paper>
+        <DisplayStoryPostTimeLineItem {...rest} />
+      </Paper>
+    </Box>
+  );
+};
+
+const mockUser = createMockUser({ name: 'user' });
+const mockStoryPost = createMockStoryPost({
+  content: `
+  # Title
+
+## SubTitle
+
+hoge**hoge**
+
+- foo
+- bar
+- baz
+
+\`\`\`ts
+const hoge = () => console.log('hoge')
+hoge()
+\`\`\`
+`,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  currentUser: mockUser,
+  storyPost: mockStoryPost,
+};
--- src/components/domains/storyPost/DisplayStoryPostTimeLineItem/DisplayStoryPostTimeLineItem.tsx
+++ src/components/domains/storyPost/DisplayStoryPostTimeLineItem/DisplayStoryPostTimeLineItem.tsx
@@ -1,7 +1,14 @@
 import React, { VFC } from 'react';
-import { Box } from '@mui/system';
+import { Box, styled } from '@mui/system';
+
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+import { IconButton, Link, Typography } from '~/components/parts/commons';
 import { StoryPost, User } from '~/domains';
-import { IconButton, Link } from '~/components/parts/commons';
+import 'github-markdown-css';
 
 type Props = {
   currentUser: User;
@@ -10,14 +17,61 @@ type Props = {
 
 export const DisplayStoryPostTimeLineItem: VFC<Props> = ({ currentUser, storyPost }) => {
   return (
-    <Box>
-      <Box>
+    <Box width="100%">
+      <StyledBox width="100%" display="flex" alignItems="center" mb="12px">
         <Link href={'/user/' + currentUser._id}>{currentUser.name}</Link>
-        <time dateTime={String(storyPost.createdAt)}>{String(storyPost.createdAt)}</time>
-        <IconButton icon="MoreVert" width={20} />
-      </Box>
-      <Box>{storyPost.content}</Box>
-      <Box></Box>
+        <Typography variant="caption">{storyPost.createdAt.toLocaleDateString()}</Typography>
+        <StyledIconButton icon="MoreVert" width={20} />
+      </StyledBox>
+      <StyledMarkdownBody className="markdown-body">
+        <ReactMarkdown
+          components={{
+            code({ inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter style={vscDarkPlus} language={match[1]} PreTag="div">
+                  {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+          plugins={[gfm]}
+          unwrapDisallowed={false}
+        >
+          {storyPost.content}
+        </ReactMarkdown>
+      </StyledMarkdownBody>
     </Box>
   );
 };
+
+const StyledBox = styled(Box)`
+  .MuiLink-root {
+    color: ${(props) => props.theme.palette.textColor.main};
+    margin-right: 8px;
+  }
+`;
+
+const StyledIconButton = styled(IconButton)`
+  margin-left: auto;
+`;
+
+const StyledMarkdownBody = styled(Box)`
+  &.markdown-body {
+    color: ${(props) => props.theme.palette.textColor.main};
+    background-color: #fff;
+    p {
+      white-space: pre-wrap;
+    }
+    pre {
+      background-color: rgb(30, 30, 30);
+    }
+    code {
+      color: #fff;
+    }
+  }
+`;
