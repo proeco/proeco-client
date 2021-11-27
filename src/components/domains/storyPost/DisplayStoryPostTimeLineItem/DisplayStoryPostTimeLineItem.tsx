37b413f696ea51b53ac7219f348ad2b521ffef67
kanoi-y <furafuratonbo@gmail.com>
2021-11-27 23:01:09 +0900

    🔨 EmojiRadioGroupを追加

--- src/components/domains/storyPost/DisplayStoryPostTimeLineItem/DisplayStoryPostTimeLineItem.tsx
+++ src/components/domains/storyPost/DisplayStoryPostTimeLineItem/DisplayStoryPostTimeLineItem.tsx
@@ -6,22 +6,25 @@ import gfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
-import { Icon, IconButton, Link, Menu, Typography, Editor } from '~/components/parts/commons';
+import { Icon, IconButton, Link, Menu, Typography, Editor, EmojiRadioGroup } from '~/components/parts/commons';
 import { StoryPost, User } from '~/domains';
 import 'github-markdown-css';
 
 type Props = {
   currentUser: User;
   storyPost: StoryPost;
+  emojiIds?: string[];
 };
 
-export const DisplayStoryPostTimeLineItem: VFC<Props> = ({ currentUser, storyPost }) => {
+export const DisplayStoryPostTimeLineItem: VFC<Props> = ({ currentUser, storyPost, emojiIds = ['thumbsup', 'heart', 'laughing', 'partying_face'] }) => {
   const [content, setContent] = useState(storyPost.content);
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
 
   const [isUpdate, setIsUpdate] = useState(false);
+
+  const [SelectedEmojiId, setSelectedEmojiId] = useState('thumbsup');
 
   const handleClickCancelButton = () => {
     setContent(storyPost.content);
@@ -55,6 +58,10 @@ export const DisplayStoryPostTimeLineItem: VFC<Props> = ({ currentUser, storyPos
     },
   ];
 
+  const handleClickEmoji = (id: string) => {
+    setSelectedEmojiId(id);
+  };
+
   return (
     <Box width="100%">
       <StyledBox width="100%" display="flex" alignItems="center" mb="12px">
@@ -66,28 +73,31 @@ export const DisplayStoryPostTimeLineItem: VFC<Props> = ({ currentUser, storyPos
       {isUpdate ? (
         <Editor isUpdateMode content={content} onChangeContent={setContent} onCompleteEdit={handleCompleteEdit} onClickCancelButton={handleClickCancelButton} />
       ) : (
-        <StyledMarkdownBody className="markdown-body">
-          <ReactMarkdown
-            components={{
-              code({ inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || '');
-                return !inline && match ? (
-                  <SyntaxHighlighter style={vscDarkPlus} language={match[1]} PreTag="div">
-                    {String(children).replace(/\n$/, '')}
-                  </SyntaxHighlighter>
-                ) : (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
-            plugins={[gfm]}
-            unwrapDisallowed={false}
-          >
-            {content}
-          </ReactMarkdown>
-        </StyledMarkdownBody>
+        <>
+          <StyledMarkdownBody className="markdown-body">
+            <ReactMarkdown
+              components={{
+                code({ inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || '');
+                  return !inline && match ? (
+                    <SyntaxHighlighter style={vscDarkPlus} language={match[1]} PreTag="div">
+                      {String(children).replace(/\n$/, '')}
+                    </SyntaxHighlighter>
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+              plugins={[gfm]}
+              unwrapDisallowed={false}
+            >
+              {content}
+            </ReactMarkdown>
+          </StyledMarkdownBody>
+          <EmojiRadioGroup emojiIds={emojiIds} selectedEmojiId={SelectedEmojiId} onClick={handleClickEmoji} />
+        </>
       )}
     </Box>
   );
@@ -105,6 +115,7 @@ const StyledIconButton = styled(IconButton)`
 `;
 
 const StyledMarkdownBody = styled(Box)`
+  margin-bottom: 20px;
   &.markdown-body {
     color: ${(props) => props.theme.palette.textColor.main};
     background-color: #fff;
