d8f689a711e96d17344e2a6cd989f348f810c93c
itizawa <itizawa.pen@gmail.com>
2021-11-27 14:41:39 +0900

    🏷️ update team

--- src/domains/team.ts
+++ src/domains/team.ts
@@ -1,13 +1,17 @@
 export class Team {
   _id: string;
+  productId: string;
+  url: string;
   name: string;
   description: string;
   adminUserId: string;
-  iconImageId: string;
+  iconImageId?: string;
   createdAt: Date;
   updatedAt: Date;
   constructor(init: Team) {
     this._id = init._id;
+    this.productId = init.productId;
+    this.url = init.url;
     this.name = init.name;
     this.description = init.description;
     this.adminUserId = init.adminUserId;
