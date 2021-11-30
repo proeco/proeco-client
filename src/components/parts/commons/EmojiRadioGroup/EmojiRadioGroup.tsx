bb68b67ec39d2263394dd6a0875155c60915122b
kanoi-y <furafuratonbo@gmail.com>
2021-11-30 22:49:37 +0900

    🎨 全て絵文字が選択されていない時に絵文字が灰色にならないように実装

--- src/components/parts/commons/EmojiRadioGroup/EmojiRadioGroup.tsx
+++ src/components/parts/commons/EmojiRadioGroup/EmojiRadioGroup.tsx
@@ -6,31 +6,45 @@ import { Emoji } from '~/components/parts/commons';
 type Props = {
   emojiIds: string[];
   onClick: (id: string) => void;
-  selectedEmojiId: string;
+  selectedEmojiId?: string;
 };
 
 export const EmojiRadioGroup: VFC<Props> = ({ emojiIds, onClick, selectedEmojiId }) => {
   return (
     <Box display="flex" alignItems="center" gap="12px">
-      {emojiIds.map((emojiId, i) => (
-        <StyledBox key={i} selected={selectedEmojiId === emojiId}>
-          <Emoji emojiId={emojiId} size={24} onClick={() => onClick(emojiId)} />
-        </StyledBox>
-      ))}
+      {emojiIds.map((emojiId, i) => {
+        if (selectedEmojiId) {
+          return (
+            <AfterSelectedEmojiBox key={i} selected={selectedEmojiId === emojiId}>
+              <Emoji emojiId={emojiId} size={20} onClick={() => onClick(emojiId)} />
+            </AfterSelectedEmojiBox>
+          );
+        }
+        return (
+          <StyledBox key={i}>
+            <Emoji emojiId={emojiId} size={20} onClick={() => onClick(emojiId)} />
+          </StyledBox>
+        );
+      })}
     </Box>
   );
 };
 
-const StyledBox = styled(Box)<{ selected: boolean }>`
+const StyledBox = styled(Box)`
   cursor: pointer;
   padding: 8px;
+  transition: all ease 100ms;
+  &:hover {
+    transform: scale(1.35);
+  }
+`;
+
+const AfterSelectedEmojiBox = styled(StyledBox)<{ selected: boolean }>`
   filter: ${(props) => (props.selected ? 'grayscale(0%)' : 'grayscale(100%)')};
   -webkit-filter: ${(props) => (props.selected ? 'grayscale(0%)' : 'grayscale(100%)')};
   transform: ${(props) => (props.selected ? 'scale(1.35)' : 'scale(1)')};
-  transition: all ease 100ms;
   &:hover {
     filter: grayscale(0%);
     -webkit-filter: grayscale(0%);
-    transform: scale(1.35);
   }
 `;
