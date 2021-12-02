3bc737a98a365c1258bede491c14b99993490cb0
itizawa <itizawa.pen@gmail.com>
2021-12-02 23:13:04 +0900

    ♻️ useStoryPosts

--- src/stores/storyPost/useStoryPosts.tsx
+++ src/stores/storyPost/useStoryPosts.tsx
@@ -2,7 +2,6 @@ import useSWR, { SWRResponse } from 'swr';
 
 import { restClient } from '~/utils/rest-client';
 import { StoryPost } from '~/domains';
-import { PaginationResult } from '~/interfaces';
 
 /**
  * 複数ストーリーを取得するSWR
@@ -11,17 +10,9 @@ import { PaginationResult } from '~/interfaces';
  * @returns error エラー
  * @returns mutate データの更新関数
  */
-export const useStoryPosts = ({
-  storyId,
-  page,
-  limit,
-}: {
-  storyId?: string;
-  page: number;
-  limit: 10;
-}): SWRResponse<PaginationResult<StoryPost>, Error> => {
+export const useStoryPosts = ({ storyId, page, limit }: { storyId?: string; page: number; limit: 10 }): SWRResponse<StoryPost[], Error> => {
   const key = storyId ? `/story-posts?storyId=${storyId}&page=${page}&limit=${limit}` : null;
-  return useSWR(key, (endpoint: string) => restClient.apiGet(endpoint).then((result) => result.data), {
+  return useSWR(key, (endpoint: string) => restClient.apiGet(endpoint).then((result) => result.data.docs), {
     revalidateOnFocus: false,
     revalidateOnReconnect: true,
   });
