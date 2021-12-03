26798cec7bb749442d5d90079532556931de3181
kanoi-y <furafuratonbo@gmail.com>
2021-12-03 13:51:04 +0900

    🎨 mockを追加

--- src/mocks/api/reaction.ts
+++ src/mocks/api/reaction.ts
@@ -1,5 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { createMockReaction } from '../domains';
+
+const db = [createMockReaction()];
+
+export const getReaction = (req: any, res: any, ctx: any) => {
+  return res(ctx.status(200), ctx.json(db[0]));
+};
 
 export const postReaction = (req: any, res: any, ctx: any) => {
+  return res(ctx.status(200));
+};
+
+export const putReaction = (req: any, res: any, ctx: any) => {
   return res(ctx.status(200));
 };
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,7 @@ import { rest } from 'msw';
 import { getAttachmentById } from './api/attachment';
 import { getCurrentUser } from './api/user';
 import { getStoryPosts, postStoryPost, deleteStoryPost } from './api/storyPost';
-import { postReaction } from './api/reaction';
+import { getReaction, postReaction, putReaction } from './api/reaction';
 
 const generateRoute = (path: string) => {
   return `${process.env.NEXT_PUBLIC_BACKEND_URL_FROM_CLIENT}/api/v1${path}`;
@@ -12,7 +12,9 @@ export const handlers = [
   rest.get(generateRoute('/users/me'), getCurrentUser),
   rest.get(generateRoute('/attachments/:id/signedUrl'), getAttachmentById),
   rest.get(generateRoute('/story-posts'), getStoryPosts),
+  rest.get(generateRoute('/reactions'), getReaction),
   rest.post(generateRoute('/story-posts'), postStoryPost),
   rest.post(generateRoute('/reactions'), postReaction),
+  rest.put(generateRoute('/reactions'), putReaction),
   rest.delete(generateRoute('/story-posts/:id'), deleteStoryPost),
 ];
