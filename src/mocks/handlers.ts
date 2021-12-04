a326039d72a438acf0ee1aad7b1ab8a516bb0b17
itizawa <itizawa.pen@gmail.com>
2021-12-04 11:06:16 +0900

    ✨ add post

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 import { getAttachmentById } from './api/attachment';
 import { getCurrentUser } from './api/user';
-import { getStories } from './api/stories';
+import { getStories, postStories } from './api/stories';
 import { getStoryPosts, postStoryPost, deleteStoryPost } from './api/storyPost';
 import { getReaction, postReaction, putReaction } from './api/reaction';
 
@@ -11,11 +11,16 @@ const generateRoute = (path: string) => {
 
 export const handlers = [
   rest.get(generateRoute('/users/me'), getCurrentUser),
+
   rest.get(generateRoute('/attachments/:id/signedUrl'), getAttachmentById),
+
   rest.get(generateRoute('/stories'), getStories),
+  rest.post(generateRoute('/stories'), postStories),
+
   rest.get(generateRoute('/story-posts'), getStoryPosts),
   rest.post(generateRoute('/story-posts'), postStoryPost),
   rest.delete(generateRoute('/story-posts/:id'), deleteStoryPost),
+
   rest.get(generateRoute('/reactions'), getReaction),
   rest.post(generateRoute('/reactions'), postReaction),
   rest.put(generateRoute('/reactions'), putReaction),
