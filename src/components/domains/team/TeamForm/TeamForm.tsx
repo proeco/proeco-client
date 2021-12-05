11f43009720ccbda760f7380f40595ea00194920
itizawa <itizawa.pen@gmail.com>
2021-12-05 19:23:54 +0900

    🚚 rename

--- src/components/domains/team/TeamForm/TeamForm.tsx
+++ src/components/domains/team/TeamForm/TeamForm.tsx
@@ -2,19 +2,21 @@ import { useRouter } from 'next/router';
 import { VFC, useState, ChangeEvent, useEffect } from 'react';
 
 import { Box } from '@mui/system';
+import { Grid } from '@mui/material';
+import { TeamCard } from '../TeamCard';
 import { Attachment, Team, User } from '~/domains';
 import { TextField, Typography, Button, Icon, Paper, IconUpload } from '~/components/parts/commons';
 import { restClient } from '~/utils/rest-client';
 import { useSuccessNotification } from '~/hooks/useSuccessNotification';
 import { useErrorNotification } from '~/hooks/useErrorNotification';
-import { URLS } from '~/constants';
+import { COLORS, URLS } from '~/constants';
 import { useTeamsRelatedUser } from '~/stores/team';
 
 type Props = {
   currentUser: User;
 };
 
-export const TeamFormPaper: VFC<Props> = ({ currentUser }) => {
+export const TeamForm: VFC<Props> = ({ currentUser }) => {
   const router = useRouter();
   const { notifySuccessMessage } = useSuccessNotification();
   const { notifyErrorMessage } = useErrorNotification();
@@ -71,37 +73,53 @@ export const TeamFormPaper: VFC<Props> = ({ currentUser }) => {
   }, [team]);
 
   return (
-    <Paper square>
-      <Box display="flex" justifyContent="center">
-        <IconUpload onSelectImage={handleChangeFile} currentImagePath={iconImage ? URL.createObjectURL(iconImage) : undefined} />
-      </Box>
-      <Typography mb={1} variant="body1" color="textColor.light">
-        プロダクトの url
-      </Typography>
-      <TextField fullWidth multiline value={team.url} onChange={(e) => updateStoryForm({ url: e.target.value })} />
-      <Typography mt={2} mb={1} variant="body1" color="textColor.light">
-        Product Id
-      </Typography>
-      <TextField fullWidth multiline value={team.productId} onChange={(e) => updateStoryForm({ productId: e.target.value })} />
-      <Typography mt={2} mb={1} variant="body1" color="textColor.light">
-        名前
-      </Typography>
-      <TextField fullWidth multiline value={team.name} onChange={(e) => updateStoryForm({ name: e.target.value })} />
-      <Typography mt={2} mb={1} variant="body1" color="textColor.light">
-        どんなプロダクト？
-      </Typography>
-      <TextField fullWidth multiline value={team.description} rows={4} onChange={(e) => updateStoryForm({ description: e.target.value })} />
-      <Box mt={4} textAlign="center">
-        <Button
-          disabled={isCreating || !isValidForm || !iconImage}
-          color="primary"
-          variant="contained"
-          startIcon={<Icon icon="CreateOutlined" width="20px" />}
-          onClick={handleClickCreateNewTeam}
-        >
-          新規チームを作成する
-        </Button>
-      </Box>
-    </Paper>
+    <Grid container>
+      <Grid xs={12} md={6} px={2} pb={3}>
+        <Paper square>
+          <Box display="flex" justifyContent="center">
+            <IconUpload onSelectImage={handleChangeFile} currentImagePath={iconImage ? URL.createObjectURL(iconImage) : undefined} />
+          </Box>
+          <Typography mb={1} variant="body1" color="textColor.light">
+            プロダクトの url
+          </Typography>
+          <TextField fullWidth multiline value={team.url} onChange={(e) => updateStoryForm({ url: e.target.value })} />
+          <Typography mt={2} mb={1} variant="body1" color="textColor.light">
+            Product Id
+          </Typography>
+          <TextField fullWidth multiline value={team.productId} onChange={(e) => updateStoryForm({ productId: e.target.value })} />
+          <Typography mt={2} mb={1} variant="body1" color="textColor.light">
+            名前
+          </Typography>
+          <TextField fullWidth multiline value={team.name} onChange={(e) => updateStoryForm({ name: e.target.value })} />
+          <Typography mt={2} mb={1} variant="body1" color="textColor.light">
+            どんなプロダクト？
+          </Typography>
+          <TextField
+            fullWidth
+            multiline
+            value={team.description}
+            rows={4}
+            onChange={(e) => updateStoryForm({ description: e.target.value })}
+          />
+          <Box mt={4} textAlign="center">
+            <Button
+              disabled={isCreating || !isValidForm || !iconImage}
+              color="primary"
+              variant="contained"
+              startIcon={<Icon icon="CreateOutlined" width="20px" />}
+              onClick={handleClickCreateNewTeam}
+            >
+              新規チームを作成する
+            </Button>
+          </Box>
+        </Paper>
+      </Grid>
+      <Grid xs={12} md={6} px={2} pb={3}>
+        <Typography variant="h4" align="center" color={COLORS.TEXT} mb={2}>
+          プレビュー
+        </Typography>
+        <TeamCard name={team.name} url={team.url} description={team.description} />
+      </Grid>
+    </Grid>
   );
 };
