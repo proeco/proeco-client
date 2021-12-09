fccb784e301cfcb20d35aa9f943fadc2bdfdb270
itizawa <itizawa.pen@gmail.com>
2021-12-09 21:12:24 +0900

    🚚 rename

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -4,7 +4,7 @@ export const URLS = {
   DASHBOARD_TEAMS: '/dashboard/teams',
   DASHBOARD_TEAMS_NEW: '/dashboard/teams/new',
   DASHBOARD_SETTINGS: '/dashboard/settings',
-  TEAMS: (teamId: string) => `/${teamId}`,
+  TEAMS: (productId: string) => `/${productId}`,
   TEAMS_STORIES: (teamId: string) => `/${teamId}/story`,
   TEAMS_STORY: (teamId: string, storyId: string, storyPostId?: string) =>
     `/${teamId}/story/${storyId}${storyPostId ? `?storyPostId=${storyPostId}` : ''}`,
