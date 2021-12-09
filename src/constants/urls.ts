d4127173c5a0afb4281b86eadb87452a20508aff
itizawa <itizawa.pen@gmail.com>
2021-12-09 21:17:30 +0900

    🔥 remove link

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -5,10 +5,8 @@ export const URLS = {
   DASHBOARD_TEAMS_NEW: '/dashboard/teams/new',
   DASHBOARD_SETTINGS: '/dashboard/settings',
   TEAMS: (productId: string) => `/${productId}`,
-  TEAMS_STORIES: (productId: string) => `/${productId}/story`,
   TEAMS_STORY: (productId: string, storyId: string, storyPostId?: string) =>
     `/${productId}/story/${storyId}${storyPostId ? `?storyPostId=${storyPostId}` : ''}`,
-  TEAMS_SETTING: (productId: string) => `/${productId}/settings`,
   TERMS: '#',
   PRIVACY_POLICY: '#',
 };
