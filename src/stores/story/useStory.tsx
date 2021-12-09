1b5ead42a4823886826699ad50d4ada7f7d35fcf
itizawa <itizawa.pen@gmail.com>
2021-12-09 21:59:25 +0900

    🐛 fix useStory

--- src/stores/story/useStory.tsx
+++ src/stores/story/useStory.tsx
@@ -11,12 +11,8 @@ import { Story } from '~/domains';
  * @returns error エラー
  * @returns mutate データの更新関数
  */
-export const useStory = (id?: string, fallbackData?: Story): SWRResponse<Story, Error> => {
-  return useImmutableSWR(
-    id ? `/stories/${id}` : null,
-    (endpoint: string) => restClient.apiGet<Story>(endpoint).then((result) => result.data),
-    {
-      fallbackData,
-    },
-  );
+export const useStory = (id: string, fallbackData: Story): SWRResponse<Story, Error> => {
+  return useImmutableSWR(`/stories/${id}`, (endpoint: string) => restClient.apiGet<Story>(endpoint).then((result) => result.data), {
+    fallbackData,
+  });
 };
