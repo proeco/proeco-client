a3a6dd0b19f2a9e44304ad84efd811ac8ad70b11
kanoi-y <furafuratonbo@gmail.com>
2021-12-09 20:37:42 +0900

    🎨 domains/story.tsを同期

--- src/domains/story.ts
+++ src/domains/story.ts
@@ -1,12 +1,16 @@
+import { Reaction } from './reaction';
+
 export class Story {
   _id: string;
   title: string;
   emojiId: string;
   teamId: string;
   isPrivate: boolean;
+  isCompleted: boolean;
   createdUserId: string;
   createdAt: Date;
   updatedAt: Date;
+  reactions?: Reaction[];
 
   constructor(init: Story) {
     this._id = init._id;
@@ -14,6 +18,7 @@ export class Story {
     this.emojiId = init.emojiId;
     this.teamId = init.teamId;
     this.isPrivate = init.isPrivate;
+    this.isCompleted = init.isCompleted;
     this.createdUserId = init.createdUserId;
     this.createdAt = init.createdAt;
     this.updatedAt = init.updatedAt;
