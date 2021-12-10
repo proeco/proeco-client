d144c785080b4346766e507dae83ef5fc3562a63
kanoi-y <furafuratonbo@gmail.com>
2021-12-10 19:14:09 +0900

    🔨 propsにstoryを追加

--- src/components/domains/story/StoryCard/StoryCard.tsx
+++ src/components/domains/story/StoryCard/StoryCard.tsx
@@ -1,11 +1,39 @@
 import React, { VFC } from 'react';
-import { Card as MuiCard } from '@mui/material';
+import { Box, Card as MuiCard, Chip, Skeleton } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { Story } from '~/domains';
+import { Emoji, Typography } from '~/components/parts/commons';
+import { useTeam } from '~/stores/team';
+import { TeamIcon, SkeltonTeamIcon } from '~/components/domains/team/TeamIcon';
 
-type Props = {};
+type Props = {
+  story: Story;
+};
 
-export const StoryCard: VFC<Props> = ({ ...rest }) => {
-  return <StyledStoryCard {...rest} />;
+export const StoryCard: VFC<Props> = ({ story }) => {
+  const { data: team } = useTeam({ teamId: story.teamId });
+
+  return (
+    <StyledStoryCard>
+      <Chip label={story.isCompleted ? 'Close' : 'Open'} />
+      <Box>
+        <Emoji emojiId={story.emojiId} size={48} />
+      </Box>
+      <Box>
+        {team ? (
+          <>
+            <TeamIcon size={20} attachmentId={team.iconImageId} />
+            <Typography>{team.name}</Typography>
+          </>
+        ) : (
+          <>
+            <SkeltonTeamIcon size={20} />
+            <Skeleton variant="text" width="100%" />
+          </>
+        )}
+      </Box>
+    </StyledStoryCard>
+  );
 };
 
 const StyledStoryCard = styled(MuiCard)`
