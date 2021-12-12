e40229d475bdb14cad98a2ab30793b48866534c1
itizawa <itizawa.pen@gmail.com>
2021-12-13 01:52:13 +0900

    ✨ implement auth0

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,3 +1,25 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback, getSession } from '@auth0/nextjs-auth0';
+import { restClient } from '~/utils/rest-client';
 
-export default handleAuth();
+export default handleAuth({
+  async callback(req, res) {
+    try {
+      await handleCallback(req, res);
+      const session = getSession(req, res);
+
+      if (!session) return;
+      const { user, accessToken } = session;
+      if (!user || !accessToken) return;
+
+      restClient.apiPost('/users', {
+        user: {
+          name: user.name,
+          email: user.email,
+          accessToken,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+});
