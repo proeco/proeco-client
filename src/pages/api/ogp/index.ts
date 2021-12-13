b6c47a1f524c0eb6ddf1d5007c9150d197013317
kanoi-y <furafuratonbo@gmail.com>
2021-12-13 12:41:26 +0900

    ✨ ogp/index.tsを作成

---
+++ src/pages/api/ogp/index.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createCanvas } from 'canvas';
+
+const WIDTH = 1200 as const;
+const HEIGHT = 630 as const;
+
+const createOgp = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  const { title } = req.query;
+
+  const canvas = createCanvas(WIDTH, HEIGHT);
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = '#FFF';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.font = '60px ipagp';
+  ctx.fillStyle = '#000000';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  const text = String(title);
+  ctx.fillText(text, WIDTH / 2, HEIGHT / 2);
+  const buffer = canvas.toBuffer();
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': buffer.length,
+  });
+  res.end(buffer, 'binary');
+};
+
+export default createOgp;
