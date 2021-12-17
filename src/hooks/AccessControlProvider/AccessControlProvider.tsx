a801f6db4c5b45ee4cdd6b014f03aaae0b4aa9ff
itizawa <itizawa.pen@gmail.com>
2021-12-17 19:15:32 +0900

    ✨ implement AccessControlProvider

---
+++ src/hooks/AccessControlProvider/AccessControlProvider.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+import { useEffect, FC } from 'react';
+import { useUser } from '@auth0/nextjs-auth0';
+import { GetAccessControl } from '~/interfaces/accessControl';
+
+const accessControl = () => {
+  throw new Error('getAccessControl が定義されていません。');
+};
+
+/**
+ * AccessControlによってログイン状態をもとにリダイレクトを行う
+ * @param getAccessControl
+ * @returns
+ */
+export const AccessControlProvider: FC<{ getAccessControl?: GetAccessControl }> = ({ getAccessControl = accessControl, children }) => {
+  const router = useRouter();
+  const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    const control = async () => {
+      if (isLoading) return;
+      const accessControl = getAccessControl();
+
+      if (accessControl.loginRequired === true && !user) {
+        router[accessControl.type](accessControl.destination);
+      } else if (accessControl.loginRequired === false && user) {
+        router[accessControl.type](accessControl.destination);
+      }
+    };
+    control();
+  }, [getAccessControl, isLoading, router, user]);
+
+  return <>{children}</>;
+};
