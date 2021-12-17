60841396484a1a83ed838331e968dbfaf7b97079
itizawa <itizawa.pen@gmail.com>
2021-12-17 20:44:08 +0900

    💄 hide

--- src/components/parts/layout/DashboardLayout/DashboardLayout.tsx
+++ src/components/parts/layout/DashboardLayout/DashboardLayout.tsx
@@ -1,13 +1,17 @@
 import { Box, styled } from '@mui/system';
 import { FC } from 'react';
 import { UserSideBar } from '../../../domains/user/UserSideBar';
+import { useCurrentUser } from '~/stores/user/useCurrentUser';
 
 export const DashboardLayout: FC = ({ children }) => {
+  const { data: currentUser } = useCurrentUser();
   return (
     <Box display="flex">
-      <Box flexShrink={1}>
-        <UserSideBar />
-      </Box>
+      {currentUser && (
+        <Box flexShrink={1}>
+          <UserSideBar />
+        </Box>
+      )}
       <StyledBox width="100%" minWidth="0" p={5}>
         {children}
       </StyledBox>
