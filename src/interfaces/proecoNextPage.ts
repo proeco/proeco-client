5749528cff1cd80bb68ad562853fd3900f52e1ab
itizawa <itizawa.pen@gmail.com>
2021-12-17 18:53:31 +0900

    🏷️ add type

--- src/interfaces/proecoNextPage.ts
+++ src/interfaces/proecoNextPage.ts
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
 import { ReactNode } from 'react';
+import { GetAccessControl } from './accessControl';
 
-export type ProecoNextPage<T = {}> = NextPage<T> & { getLayout: (page: ReactNode) => JSX.Element };
+export type ProecoNextPage<T = {}> = NextPage<T> & { getLayout: (page: ReactNode) => JSX.Element; getAccessControl?: GetAccessControl };
