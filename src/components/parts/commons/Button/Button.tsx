30256f53be43d8983c01164d9f73c80553737096
itizawa <itizawa.pen@gmail.com>
2021-12-19 13:45:22 +0900

    ✨ display button

--- src/components/parts/commons/Button/Button.tsx
+++ src/components/parts/commons/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { VFC, ComponentProps } from 'react';
+import React, { FC, ComponentProps } from 'react';
 import { styled } from '@mui/material/styles';
 import { Button as MuiButton } from '@mui/material';
 
@@ -8,8 +8,15 @@ type Bold = {
 
 type Props = ComponentProps<typeof MuiButton> & Bold;
 
-export const Button: VFC<Props> = ({ bold, ...rest }) => {
-  return <StyledButton {...rest} bold={bold ? 1 : 0} />;
+export const Button: FC<Props> = ({ bold, children, ...rest }) => {
+  return (
+    <>
+      <StyledButton {...rest} bold={bold ? 1 : 0}>
+        children
+      </StyledButton>
+      <button className="btn btn-primary bg-primary">{children}</button>
+    </>
+  );
 };
 
 const StyledButton = styled(MuiButton)<{ bold: number }>`
