914450be583926655ae095925748a6ebebf306fd
kanoi-y <furafuratonbo@gmail.com>
2021-12-19 16:03:42 +0900

    ✨ StoryTab.stories.tsxを作成

--- src/mocks/api/stories.ts
+++ src/mocks/api/stories.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createMockStory } from '../domains';
 
-const db = [createMockStory()];
+const db = [createMockStory({ isCompleted: true }), createMockStory({ isCompleted: false })];
 
 export const getStories = (req: any, res: any, ctx: any) => {
-  return res(ctx.status(200), ctx.json(db[0]));
+  return res(ctx.status(200), ctx.json({ docs: db }));
 };
 
 export const postStories = (req: any, res: any, ctx: any) => {
