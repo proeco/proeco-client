cf66a09b4e05b0b93cae954e724e0b08739d830f
kanoi-y <furafuratonbo@gmail.com>
2021-12-19 13:58:49 +0900

    🎨 進行中と完了したストーリーを表示できるように実装

--- src/components/domains/story/StoryTab/StoryTab.tsx
+++ src/components/domains/story/StoryTab/StoryTab.tsx
@@ -1,10 +1,12 @@
-import React, { VFC, useState } from 'react';
+import React, { VFC, useState, ChangeEvent } from 'react';
 import { TabPanel } from '@mui/lab';
-import { Box, styled } from '@mui/material';
+import { Box, Grid, styled } from '@mui/material';
 import { Button, Icon, Typography, Pagination } from '~/components/parts/commons';
 import { StoryListTable } from '~/components/domains/story/StoryListTable';
 import { CreateNewStoryModal } from '~/components/domains/story/CreateNewStoryModal';
+import { StoryCard, SkeltonStoryCard } from '~/components/domains/story/StoryCard';
 import { Team } from '~/domains';
+import { useStories } from '~/stores/story';
 
 const TabTypes = { HOME: 'home', STORY: 'story', SETTINGS: 'settings' };
 type TabTypes = typeof TabTypes[keyof typeof TabTypes];
@@ -14,11 +16,16 @@ type Props = {
   limit: 10;
   team: Team;
   count: number;
-  onChangePage: () => void;
+  isMemberOfTeam: boolean;
+  onChangePage: (event: ChangeEvent<unknown>, value: number | null) => void;
 };
 
-export const StoryTab: VFC<Props> = ({ page, limit, team, count, onChangePage }) => {
+export const StoryTab: VFC<Props> = ({ page, limit, team, count, isMemberOfTeam, onChangePage }) => {
   const [isOpenCreateNewStoryModal, setIsOpeCreateNewStoryModal] = useState(false);
+
+  const { data: storyList } = useStories({ page, limit, teamId: team._id });
+  const openStoryList = storyList ? storyList?.docs.filter((story) => story.isCompleted === false) : [];
+  const closeStoryList = storyList ? storyList?.docs.filter((story) => story.isCompleted === true) : [];
 
   const handleClickCreateStoryButton = () => {
     setIsOpeCreateNewStoryModal(true);
@@ -26,16 +33,46 @@ export const StoryTab: VFC<Props> = ({ page, limit, team, count, onChangePage })
 
   return (
     <TabPanel value={TabTypes.STORY}>
-      <Box mb={2} display="flex" alignItems="center" justifyContent="space-between">
+      <Box mb={6} display="flex" alignItems="center" justifyContent="space-between">
         <Typography variant="h3" bold display="flex" alignItems="center" gap="8px">
           <Icon icon="HistoryEdu" width={32} />
           ストーリーリスト
         </Typography>
-        <Button variant="contained" bold onClick={handleClickCreateStoryButton} startIcon={<Icon icon="CreateOutlined" width="20px" />}>
-          ストーリーを追加する
-        </Button>
+        {isMemberOfTeam && (
+          <Button variant="contained" bold onClick={handleClickCreateStoryButton} startIcon={<Icon icon="CreateOutlined" width="20px" />}>
+            ストーリーを追加する
+          </Button>
+        )}
       </Box>
-      <StoryListTable page={page} limit={limit} teamId={team._id} productId={team.productId} />
+      <Typography variant="h4" bold align="center" mb={3}>
+        進行中のストーリー
+      </Typography>
+      <Box mb={5}>
+        <Grid container maxWidth="900px" mx="auto">
+          {openStoryList ? (
+            openStoryList.map((story) => {
+              return (
+                <Grid item key={`top-${story._id}`} xs={12} sm={6} md={4} px={1} pb={2}>
+                  <StoryCard story={story} isLink />
+                </Grid>
+              );
+            })
+          ) : (
+            <>
+              <Grid item xs={12} sm={6} md={4} px={1}>
+                <SkeltonStoryCard />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} px={1}>
+                <SkeltonStoryCard />
+              </Grid>
+            </>
+          )}
+        </Grid>
+      </Box>
+      <Typography variant="h4" bold align="center" mb={3}>
+        完了したストーリー
+      </Typography>
+      <StoryListTable stories={closeStoryList} productId={team.productId} />
       <StyledPagination count={count} page={page} onChange={onChangePage} />
       <CreateNewStoryModal
         isOpen={isOpenCreateNewStoryModal}
