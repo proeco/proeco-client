a5e7020ac031ad4a609fc2b5fe3aa69a1f7a6296
itizawa <itizawa.pen@gmail.com>
2021-12-26 00:54:34 +0900

    }; ✨ update

--- src/components/parts/commons/Dropdown/Dropdown.tsx
+++ src/components/parts/commons/Dropdown/Dropdown.tsx
@@ -3,19 +3,26 @@ import { Dropdown as DropdownOriginal, DropdownMenu, DropdownToggle, DropdownIte
 
 type Props = {
   toggle: ReactNode;
+  tag?: 'span';
 };
 
-export const Dropdown: FC<Props> = ({ toggle, children }) => {
+export const Dropdown: FC<Props> = ({ toggle, tag, children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <DropdownOriginal toggle={() => setIsOpen((prev) => !prev)} isOpen={isOpen}>
-      <DropdownToggle color="transparent">{toggle}</DropdownToggle>
+      <DropdownToggle color="transparent" tag={tag} className="c-pointer">
+        {toggle}
+      </DropdownToggle>
       <DropdownMenu>{children}</DropdownMenu>
     </DropdownOriginal>
   );
 };
 
 export const DropdownItem: FC<{ onClick?: () => void }> = ({ onClick, children }) => {
-  return <DropdownItemOOriginal onClick={onClick}>{children}</DropdownItemOOriginal>;
+  return (
+    <DropdownItemOOriginal onClick={onClick} className="d-flex align-items-center">
+      {children}
+    </DropdownItemOOriginal>
+  );
 };
