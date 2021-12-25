c11ff8ed8cb59ef78382ae680751092303f39bab
itizawa <itizawa.pen@gmail.com>
2021-12-25 12:30:24 +0900

    💄 adjust home

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/system';
 import { ReactNode } from 'react';
-import { Icon, Typography } from '~/components/parts/commons';
 import { DashboardLayout } from '~/components/parts/layout/DashboardLayout';
 import { ProecoOgpHead } from '~/components/parts/layout/ProecoOgpHead';
 import { URLS } from '~/constants';
@@ -12,10 +11,10 @@ const Dashboard: ProecoNextPage = () => {
       <ProecoOgpHead />
       <Box mx="auto" maxWidth="1200px">
         <Box mb={2} display="flex" alignItems="center" justifyContent="space-between">
-          <Typography variant="h3" bold display="flex" alignItems="center" gap="8px">
+          {/* <h3 variant="h3" bold display="flex" alignItems="center" gap="8px">
             <Icon icon="DashboardOutlined" width={32} />
             ホーム
-          </Typography>
+          </h3> */}
         </Box>
       </Box>
     </>
