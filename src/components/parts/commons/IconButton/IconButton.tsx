e5da825f6968deabf30421020d9d937fc1e73089
itizawa <itizawa.pen@gmail.com>
2021-12-25 19:22:17 +0900

    ✨ implement iconButton

--- src/components/parts/commons/IconButton/IconButton.tsx
+++ src/components/parts/commons/IconButton/IconButton.tsx
@@ -1,18 +1,58 @@
-import React, { VFC, ComponentProps } from 'react';
-import { IconButton as MuiIconButton } from '@mui/material';
-import { Icon } from '~/components/parts/commons';
+import { VFC, ComponentProps, MouseEvent } from 'react';
+import { Icon } from '..';
+import { BootstrapColor } from '~/interfaces/variables';
 
-type IconButtonType = {
-  icon: ComponentProps<typeof Icon>['icon'];
-  width: number;
-};
+type Props = {
+  isActive?: boolean;
+  disabled?: boolean;
+  isRemovePadding?: boolean;
+  text?: string;
+  color: BootstrapColor;
+  activeColor: BootstrapColor;
+  buttonSize?: 'sm' | 'lg';
+  buttonColor?: BootstrapColor;
+  onClickButton?: (e: MouseEvent<HTMLButtonElement>) => void;
+} & ComponentProps<typeof Icon>;
 
-type Props = ComponentProps<typeof MuiIconButton> & IconButtonType;
+export const IconButton: VFC<Props> = ({
+  size,
+  isActive,
+  disabled,
+  isRemovePadding = false,
+  icon,
+  text,
+  color,
+  activeColor,
+  buttonColor,
+  buttonSize,
+  onClickButton,
+}) => {
+  const textColor = isActive ? activeColor : color;
 
-export const IconButton: VFC<Props> = ({ icon, width, ...rest }) => {
+  const handleClickButton = (e: MouseEvent<HTMLButtonElement>) => {
+    if (onClickButton != null) {
+      onClickButton(e);
+    }
+  };
+
+  const btnClassName: string[] = ['btn', 'px-2', 'border-0'];
+  if (buttonSize != null) {
+    btnClassName.push(`btn-${buttonSize}`);
+  }
+  if (buttonColor != null) {
+    btnClassName.push(`btn-${BootstrapColor[buttonColor]}`);
+  }
+  if (disabled) {
+    btnClassName.push('disabled');
+  }
+  if (isRemovePadding) {
+    btnClassName.push('py-0');
+  }
+
   return (
-    <MuiIconButton {...rest}>
-      <Icon icon={icon} width={width} />
-    </MuiIconButton>
+    <button className={btnClassName.join(' ')} onClick={handleClickButton}>
+      <Icon size={size} icon={icon} color={textColor} />
+      {text && <span className={`ms-2 text-${BootstrapColor[textColor]}`}>{text}</span>}
+    </button>
   );
 };
