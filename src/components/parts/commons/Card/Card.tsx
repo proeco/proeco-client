9949da99dece2101e3905cebc012289e0a887496
kanoi-y <furafuratonbo@gmail.com>
2021-12-26 21:14:10 +0900

    💄 styleを追加

--- src/components/parts/commons/Card/Card.tsx
+++ src/components/parts/commons/Card/Card.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 
 export const Card: FC<Props> = ({ padding = 3, square = false, children }) => {
-  const classNames = ['card border-0 shadow'];
+  const classNames = ['card border-0 shadow text-black overflow-hidden'];
   if (square) classNames.push('rounded-0');
   return (
     <div className={classNames.join(' ')}>
