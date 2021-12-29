219494e1cda341b85ff998a44a7092c2fc2056b1
kanoi-y <furafuratonbo@gmail.com>
2021-12-29 21:12:22 +0900

    🎨 PaginationをBS5に移行

--- src/components/parts/commons/Pagination/Pagination.tsx
+++ src/components/parts/commons/Pagination/Pagination.tsx
@@ -1,8 +1,31 @@
-import React, { VFC, ComponentProps } from 'react';
-import { Pagination as MuiPagination } from '@mui/material';
+import React, { VFC } from 'react';
+import { Pagination as PaginationOriginal, PaginationItem, PaginationLink } from 'reactstrap';
 
-type Props = ComponentProps<typeof MuiPagination>;
+type Props = {
+  count: number;
+  page: number;
+  onChange: (value: number | null) => void;
+};
 
-export const Pagination: VFC<Props> = ({ color = 'primary', ...rest }) => {
-  return <MuiPagination color={color} {...rest} />;
+export const Pagination: VFC<Props> = ({ count, page, onChange }) => {
+  // [1, 2, 3, ..., count]のような連番の配列
+  const countArr = [...Array(count)].map((_, i) => i + 1);
+
+  return (
+    <PaginationOriginal>
+      <PaginationItem>
+        <PaginationLink previous href="#" onClick={() => onChange(page - 1 < 1 ? null : page - 1)} />
+      </PaginationItem>
+      {countArr.map((num) => (
+        <PaginationItem key={num} active={page === num}>
+          <PaginationLink href="#" onClick={() => onChange(num)}>
+            {num}
+          </PaginationLink>
+        </PaginationItem>
+      ))}
+      <PaginationItem>
+        <PaginationLink next href="#" onClick={() => onChange(page + 1 > count ? null : page + 1)} />
+      </PaginationItem>
+    </PaginationOriginal>
+  );
 };
