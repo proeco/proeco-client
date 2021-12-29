df194ccee3fff42b8e592f474b4aa22ff450bf0b
kanoi-y <furafuratonbo@gmail.com>
2021-12-29 19:15:24 +0900

    🎨 TopページにCarouselを導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
-import { Grid } from '@mui/material';
 import { ReactNode } from 'react';
 import { Box } from '@mui/system';
 
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+import styled from 'styled-components';
 import { useTeams } from '~/stores/team';
 import { useCurrentUser } from '~/stores/user/useCurrentUser';
 
@@ -16,6 +19,21 @@ import { useStories } from '~/stores/story';
 import { SkeltonStoryCard, StoryCard } from '~/components/domains/story/StoryCard';
 import { ProecoNextPage } from '~/interfaces/proecoNextPage';
 
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 960 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 960, min: 600 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 600, min: 0 },
+    items: 1,
+  },
+};
+
 const Home: ProecoNextPage = () => {
   const { data: currentUser } = useCurrentUser();
   const { data: teamList } = useTeams({ page: 1 });
@@ -25,56 +43,54 @@ const Home: ProecoNextPage = () => {
     <>
       <ProecoOgpHead />
       <h1 className="text-center fw-bold my-3">プロダクト一覧</h1>
-      <Box mb={5}>
-        <Grid container maxWidth="900px" mx="auto">
-          {teamList ? (
-            teamList.docs.map((team, index) => (
-              <Grid item key={index} xs={12} sm={6} px={1} pb={2}>
-                <Link href={URLS.TEAMS(team.productId)}>
-                  <TeamCard
-                    name={team.name}
-                    productId={team.productId}
-                    description={team.description}
-                    attachmentId={team.iconImageId}
-                    url={team.url}
-                  />
-                </Link>
-              </Grid>
-            ))
-          ) : (
-            <>
-              <Grid item xs={12} sm={6} px={1}>
-                <SkeltonTeamCard />
-              </Grid>
-              <Grid item xs={12} sm={6} px={1}>
-                <SkeltonTeamCard />
-              </Grid>
-            </>
-          )}
-        </Grid>
+      <Box mb={5} maxWidth="1200px" mx="auto">
+        <StyledCarousel responsive={responsive} showDots arrows={false} autoPlay>
+          {teamList
+            ? teamList.docs.map((team, index) => {
+                return (
+                  <Box px={2} key={index}>
+                    <Link href={URLS.TEAMS(team.productId)}>
+                      <TeamCard
+                        name={team.name}
+                        productId={team.productId}
+                        description={team.description}
+                        attachmentId={team.iconImageId}
+                        url={team.url}
+                      />
+                    </Link>
+                  </Box>
+                );
+              })
+            : [
+                <Box px={2} key="first">
+                  <SkeltonTeamCard />
+                </Box>,
+                <Box px={2} key="second">
+                  <SkeltonTeamCard />
+                </Box>,
+              ]}
+        </StyledCarousel>
       </Box>
       <h2 className="text-center fw-bold my-3">進行中のストーリー一覧</h2>
-      <Box mb={5}>
-        <Grid container maxWidth="900px" mx="auto">
-          {openStoryList ? (
-            openStoryList.docs.map((story, index) => {
-              return (
-                <Grid item key={index} xs={12} sm={6} md={4} px={1} pb={2}>
-                  <StoryCard story={story} />
-                </Grid>
-              );
-            })
-          ) : (
-            <>
-              <Grid item xs={12} sm={6} md={4} px={1}>
-                <SkeltonStoryCard />
-              </Grid>
-              <Grid item xs={12} sm={6} md={4} px={1}>
-                <SkeltonStoryCard />
-              </Grid>
-            </>
-          )}
-        </Grid>
+      <Box mb={5} maxWidth="1200px" mx="auto">
+        <StyledCarousel responsive={responsive} showDots arrows={false} autoPlay>
+          {openStoryList
+            ? openStoryList.docs.map((story, index) => {
+                return (
+                  <Box px={2} key={index}>
+                    <StoryCard story={story} />
+                  </Box>
+                );
+              })
+            : [
+                <Box px={2} key="first">
+                  <SkeltonStoryCard />
+                </Box>,
+                <Box px={2} key="second">
+                  <SkeltonStoryCard />
+                </Box>,
+              ]}
+        </StyledCarousel>
       </Box>
       <h1 className="text-center fw-bold">Top Page</h1>
       <Link href={URLS.DASHBOARD_TEAMS}>所属チーム一覧</Link>
@@ -90,6 +106,28 @@ const Home: ProecoNextPage = () => {
     </>
   );
 };
+
+const StyledCarousel = styled(Carousel)`
+  &.react-multi-carousel-list {
+    padding-bottom: 48px;
+  }
+  .react-multi-carousel-dot-list {
+    gap: 4px;
+  }
+  .react-multi-carousel-dot {
+    > button {
+      border: none;
+      background-color: #ced7fd;
+    }
+  }
+  .react-multi-carousel-dot--active {
+    > button {
+      /* あとで直す */
+      background-color: #6684f7;
+      transform: scale(1.6);
+    }
+  }
+`;
 
 Home.getLayout = (page: ReactNode) => <>{page}</>;
 
