a7b90c7f6016f949c7c44daa26e0cf4ffe7edb09
itizawa <itizawa.pen@gmail.com>
2021-12-31 07:45:20 +0900

    💄 adjust style

--- src/components/parts/layout/DashboardLayout/DashboardLayout.tsx
+++ src/components/parts/layout/DashboardLayout/DashboardLayout.tsx
@@ -7,14 +7,13 @@ import { useCurrentUser } from '~/stores/user/useCurrentUser';
 export const DashboardLayout: FC = ({ children }) => {
   const { data: currentUser } = useCurrentUser();
   return (
-    <StyledDiv className="d-flex h-100">
+    <div className="min-vh-100 d-flex h-100">
       {currentUser && <SideBar currentUser={currentUser} />}
-      <div className="w-100 p-md-4 p-3">{children}</div>
-    </StyledDiv>
+      <StyledDiv className="w-100 p-md-4 p-3">{children}</StyledDiv>
+    </div>
   );
 };
 
 const StyledDiv = styled.div`
-  min-height: 100vh;
   min-width: 0%;
 `;
