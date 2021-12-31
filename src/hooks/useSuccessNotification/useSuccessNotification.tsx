fada5c6be9bdf14cb1c44de255eda5383aca049e
itizawa <itizawa.pen@gmail.com>
2021-12-31 13:01:46 +0900

    ♻️ refactor notificationcode

--- src/hooks/useSuccessNotification/useSuccessNotification.tsx
+++ src/hooks/useSuccessNotification/useSuccessNotification.tsx
@@ -1,15 +1,24 @@
-import { useSnackbar } from 'notistack';
+import { useCallback } from 'react';
+import { success } from 'toastr';
+
+const option = {
+  closeButton: true,
+  progressBar: true,
+  newestOnTop: false,
+  showDuration: 100,
+  hideDuration: 100,
+  timeOut: 3000,
+  positionClass: 'toast-bottom-left',
+};
 
 /**
  * ユーザーにSUCCESSを知らせるためのSnackbarを表示させるhooks
  * @returns notifySuccessMessage SUCCESSのSnackbarを出す関数
  */
 export const useSuccessNotification = () => {
-  const { enqueueSnackbar } = useSnackbar();
-
-  const notifySuccessMessage = (message: string) => {
-    enqueueSnackbar(message, { variant: 'success' });
-  };
+  const notifySuccessMessage = useCallback((message: string) => {
+    success(message, 'Success', option);
+  }, []);
 
   return {
     notifySuccessMessage,
