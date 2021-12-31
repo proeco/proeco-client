518781351bae9e623a56fdba244a516e2d5a46bb
itizawa <itizawa.pen@gmail.com>
2021-12-31 18:20:24 +0900

    ⚡️ adjust NavigationBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import { ReactNode } from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { SWRConfig } from 'swr';
@@ -5,11 +6,13 @@ import { SWRConfig } from 'swr';
 import '~/styles/global.scss';
 
 import { ProecoNextPage } from '~/interfaces/proecoNextPage';
-import { NavigationBar } from '~/components/parts/layout/NavigationBar';
+
 import { CurrentUserProvider } from '~/hooks/CurrentUserProvider';
 import { AccessControlProvider } from '~/hooks/AccessControlProvider';
 
 function MyApp({ Component, pageProps }: { Component: ProecoNextPage; pageProps: { children?: ReactNode } }): JSX.Element {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const NavigationBar = dynamic(() => import('~/components/parts/layout/NavigationBar').then((v) => v.NavigationBar) as any);
   const getLayout = Component.getLayout || ((page) => <>{page}</>);
 
   if (process.env.NEXT_PUBLIC_ENABLE_MOCK === 'TRUE') {
