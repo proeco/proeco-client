c1f105e71bf692b6a3336e23079184d2cdc214a4
itizawa <itizawa.pen@gmail.com>
2021-12-31 12:47:20 +0900

    💄 adjust table

--- src/components/domains/story/StoryListTable/StoryListTable.tsx
+++ src/components/domains/story/StoryListTable/StoryListTable.tsx
@@ -1,11 +1,8 @@
 import { VFC } from 'react';
 
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-
-import styled from 'styled-components';
-import { StoryTableRow } from '~/components/domains/story/StoryTableRow';
-
 import { Story } from '~/domains';
+import { StoryTableRow } from '~/components/domains/story/StoryTableRow';
+import { Card } from '~/components/parts/commons';
 
 type Props = {
   stories: Story[];
@@ -14,33 +11,21 @@ type Props = {
 
 export const StoryListTable: VFC<Props> = ({ stories, productId }) => {
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <StyledHeaderTableCell>
-              <span className="text-light fs-3">作成者</span>
-            </StyledHeaderTableCell>
-            <StyledHeaderTableCell>
-              <span className="text-light fs-3">ストーリー名</span>
-            </StyledHeaderTableCell>
-            <StyledHeaderTableCell align="right">
-              <span className="text-light fs-3">最終更新日</span>
-            </StyledHeaderTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {stories.map((doc) => (
-            <StoryTableRow story={doc} key={doc._id} productId={productId} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Card>
+      <div className="row">
+        <div className="col-2">
+          <span className="text-light fs-3 text-nowrap">作成者</span>
+        </div>
+        <div className="col-7">
+          <span className="text-light fs-3 text-nowrap">ストーリー名</span>
+        </div>
+        <div className="col-3">
+          <span className="text-light fs-3 text-nowrap">最終更新日</span>
+        </div>
+      </div>
+      {stories.map((doc) => (
+        <StoryTableRow story={doc} key={doc._id} productId={productId} />
+      ))}
+    </Card>
   );
 };
-
-const StyledHeaderTableCell = styled(TableCell)`
-  &.MuiTableCell-root {
-    padding: 6px 16px;
-  }
-`;
