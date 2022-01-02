f1078af86c319d638110bdb4d5a563cf26e7012d
itizawa <itizawa.pen@gmail.com>
2022-01-02 21:37:59 +0900

    ✨ create FooterNavbar

---
+++ src/components/parts/layout/FooterNavbar/FooterNavbar.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { VFC } from 'react';
+
+import { IconMap } from '../../commons/Icon/Icon';
+import { Icon } from '~/components/parts/commons';
+import { URLS } from '~/constants';
+
+export const FooterNavbar: VFC = () => {
+  const router = useRouter();
+
+  const navbarItemMappings: Array<{ text: string; url: string; icon: keyof typeof IconMap }> = [
+    // { text: 'トップ', url: URLS., icon: 'HOME' },
+    { text: 'チーム', url: URLS.DASHBOARD_TEAMS, icon: 'PEOPLE' },
+    { text: '設定', url: URLS.DASHBOARD_SETTINGS, icon: 'GEAR' },
+  ];
+
+  return (
+    <div className="fixed-bottom bg-white d-flex justify-content-evenly d-md-none">
+      {navbarItemMappings.map((v) => {
+        const isActive = router.pathname.startsWith(v.url);
+        return (
+          <Link key={v.text} href={v.url}>
+            <div className="text-center col d-flex flex-column bg-dark border-top py-2 border-secondary">
+              <small className="ms-1 text-nowrap">{v.icon != null && <Icon icon={v.icon} color={isActive ? 'PRIMARY' : 'BLACK'} />}</small>
+              <span className={`ms-2 ${isActive ? 'text-primary' : 'text-dark'}`}>{v.text}</span>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
