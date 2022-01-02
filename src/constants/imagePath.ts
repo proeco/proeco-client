c5fb506e3d6ec1dcd0850d9a00d77bf542f0e451
itizawa <itizawa.pen@gmail.com>
2022-01-02 20:57:44 +0900

    ✨ upload svg

--- src/constants/imagePath.ts
+++ src/constants/imagePath.ts
@@ -1,7 +1,7 @@
 export const IMAGE_PATH = {
   LOGO: '/images/Original.svg',
   ERROR_404: '/images/Error_404_SVG.svg',
-  EYE_CATCH: '/images/eye-catch.png',
+  EYE_CATCH: '/images/eye-catch.svg',
   TEACH_404: '/images/Teach_SVG.svg',
   TWITTER_OGP: '/images/twitter-ogp.png',
   SIGN_IN_GOOGLE: '/images/sign_in_with_google.png',
