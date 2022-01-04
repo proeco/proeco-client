354e48e4b374059e32a721786e125f5f493186cc
itizawa <itizawa.pen@gmail.com>
2022-01-05 00:48:06 +0900

    ✨ implement post

--- src/pages/api/access-token.ts
+++ src/pages/api/access-token.ts
@@ -1,10 +1,18 @@
 import { getSession } from 'next-auth/react';
+import { restClient } from '~/utils/rest-client';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async (req: any, res: any) => {
   const session = await getSession({ req });
 
-  if (session) {
+  if (session && session.user) {
+    await restClient.apiPost('/users', {
+      user: {
+        name: session.user.name,
+        email: session.user.email,
+        accessToken: session.accessToken,
+      },
+    });
     res.send({
       accessToken: session.accessToken,
     });
