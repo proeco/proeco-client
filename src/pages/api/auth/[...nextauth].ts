0a615e92777ec6233d269b76ec60c7c5b43b15aa
itizawa <itizawa.pen@gmail.com>
2022-01-05 00:43:39 +0900

    ✨ jwt

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,6 +9,12 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
     async session({ session, token }) {
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken;
