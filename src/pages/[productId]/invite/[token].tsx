3f6cef81a5d2f44dc86d3de05850645f87155d2b
kanoi-y <furafuratonbo@gmail.com>
2022-01-09 18:15:14 +0900

    🔨 参加しないボタンをクリックした際にホームへ遷移するように実装

--- src/pages/[productId]/invite/[token].tsx
+++ src/pages/[productId]/invite/[token].tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from 'next';
 import { addDays, isPast } from 'date-fns';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { DashboardLayout } from '~/components/parts/layout/DashboardLayout';
 import { ProecoOgpHead } from '~/components/parts/layout/ProecoOgpHead';
 import { InvitationToken, Team } from '~/domains';
@@ -7,6 +9,9 @@ import { PaginationResult } from '~/interfaces';
 import { ProecoNextPage } from '~/interfaces/proecoNextPage';
 import { restClient } from '~/utils/rest-client';
 import { Button } from '~/components/parts/commons';
+import { LoginModal } from '~/components/parts/authentication/LoginModal';
+import { useCurrentUser } from '~/stores/user/useCurrentUser';
+import { URLS } from '~/constants';
 
 const TOKEN_LIMIT_DAYS = 7;
 
@@ -15,14 +20,30 @@ type Props = {
 };
 
 const InvitePage: ProecoNextPage<Props> = ({ team }) => {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const { data: currentUser } = useCurrentUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    currentUser ? setIsLoginModalOpen(false) : setIsLoginModalOpen(true);
+  }, [currentUser]);
+
+  const handleRejectInvite = () => {
+    router.push(URLS.TOP);
+  };
+
   return (
     <DashboardLayout>
       <ProecoOgpHead />
       <h1>{team.name}の参加確認画面</h1>
       <div className="d-flex align-items-center gap-2">
         <Button color="primary">チームに参加する</Button>
-        <Button color="primary">チームに参加しない</Button>
+        <Button color="primary" onClick={handleRejectInvite}>
+          チームに参加しない
+        </Button>
       </div>
+      <LoginModal isOpen={isLoginModalOpen} onClose={() => setIsLoginModalOpen(false)} />
     </DashboardLayout>
   );
 };
