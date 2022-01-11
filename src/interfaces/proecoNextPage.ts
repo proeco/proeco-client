725414020f75ce67b8f73a0b326a62cdea3a9948
itizawa <itizawa.pen@gmail.com>
2022-01-12 00:10:24 +0900

    🏷️ update ProecoNextPage

--- src/interfaces/proecoNextPage.ts
+++ src/interfaces/proecoNextPage.ts
@@ -1,4 +1,7 @@
 import { NextPage } from 'next';
+import { VFC } from 'react';
 import { GetAccessControl, WithGetAccessControl } from './accessControl';
 
-export type ProecoNextPage<T = {}> = WithGetAccessControl<NextPage<T> & { getAccessControl: GetAccessControl }>;
+export type ProecoNextPage<T = {}> = WithGetAccessControl<
+  NextPage<T> & { getAccessControl: GetAccessControl; generateOgp: <T>(props: T) => VFC<T> }
+>;
