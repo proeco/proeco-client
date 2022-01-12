5bc9d8a01c20002563b689bfacd68b7e713413e0
itizawa <itizawa.pen@gmail.com>
2022-01-13 08:33:32 +0900

    🐛

--- src/pages/[productId]/settings/index.tsx
+++ src/pages/[productId]/settings/index.tsx
@@ -7,7 +7,6 @@ import { ProecoOgpHead } from '~/components/parts/layout/ProecoOgpHead';
 import { TeamPageLayout } from '~/components/parts/layout/TeamPageLayout';
 
 import { useCurrentUser } from '~/stores/user/useCurrentUser';
-import { useTeamUsers } from '~/stores/team';
 
 import { restClient } from '~/utils/rest-client';
 
@@ -21,11 +20,6 @@ type Props = {
 
 const Dashboard: ProecoNextPage<Props> = ({ team }) => {
   const { data: currentUser } = useCurrentUser();
-  const { data: teamUsers = [] } = useTeamUsers({ teamId: team?._id });
-
-  const isMemberOfTeam = useMemo(() => {
-    return !!currentUser && teamUsers.some((teamUser) => teamUser._id === currentUser._id);
-  }, [currentUser, teamUsers]);
 
   if (!team) {
     return null;
@@ -34,7 +28,7 @@ const Dashboard: ProecoNextPage<Props> = ({ team }) => {
   return (
     <TeamPageLayout team={team}>
       <StyledDiv className="mx-auto py-3">
-        {isMemberOfTeam && currentUser && <TeamSettingTab currentUser={currentUser} team={team} />}
+        {currentUser && team.adminUserId === currentUser._id && <TeamSettingTab currentUser={currentUser} team={team} />}
       </StyledDiv>
     </TeamPageLayout>
   );
