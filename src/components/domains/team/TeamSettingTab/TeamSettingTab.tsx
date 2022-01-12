5a59bd78b6be8c5bdfe7473dd8f6b9daac587667
kanoi-y <furafuratonbo@gmail.com>
2022-01-12 18:47:49 +0900

    🔨 handleCreateInviteLinkを実装

--- src/components/domains/team/TeamSettingTab/TeamSettingTab.tsx
+++ src/components/domains/team/TeamSettingTab/TeamSettingTab.tsx
@@ -1,6 +1,11 @@
 import React, { VFC } from 'react';
 import { TeamForm } from '~/components/domains/team/TeamForm';
-import { Team, User } from '~/domains';
+import { Button } from '~/components/parts/commons';
+import { URLS } from '~/constants';
+import { InvitationToken, Team, User } from '~/domains';
+import { useErrorNotification } from '~/hooks/useErrorNotification';
+import { useSuccessNotification } from '~/hooks/useSuccessNotification';
+import { restClient } from '~/utils/rest-client';
 
 type Props = {
   currentUser: User;
@@ -8,12 +13,28 @@ type Props = {
 };
 
 export const TeamSettingTab: VFC<Props> = ({ currentUser, team }) => {
+  const { notifySuccessMessage } = useSuccessNotification();
+  const { notifyErrorMessage } = useErrorNotification();
+  const handleCreateInviteLink = async () => {
+    try {
+      await restClient.apiPost<InvitationToken>('/invitation-tokens', {
+        teamId: team._id,
+      });
+
+      notifySuccessMessage('招待リンクの作成に成功しました!');
+    } catch (error) {
+      notifyErrorMessage('招待リンクの作成に失敗しました!');
+    }
+  };
   return (
     <>
       <div className="row gy-3">
         <div className="list-group col-12 col-md-3">
           <a href="#" className="list-group-item list-group-item-action active" aria-current="true">
             基本設定
+          </a>
+          <a href={URLS.TEAMS_SETTINGS(team.productId) + '?tab=team'} className="list-group-item list-group-item-action" aria-current="true">
+            チーム設定
           </a>
           {/* TODO */}
           {/* <a href="#" className="list-group-item list-group-item-action">
@@ -22,6 +43,9 @@ export const TeamSettingTab: VFC<Props> = ({ currentUser, team }) => {
         </div>
         <div className="col-12 col-md-9">
           <TeamForm currentUser={currentUser} team={team} />
+          <Button color="primary" onClick={handleCreateInviteLink}>
+            招待リンクを作成
+          </Button>
         </div>
       </div>
     </>
