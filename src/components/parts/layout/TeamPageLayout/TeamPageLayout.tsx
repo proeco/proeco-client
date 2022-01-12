8bf1fc315eb5b05c2a3a31d67db6e911b219e5a6
itizawa <itizawa.pen@gmail.com>
2022-01-12 09:56:50 +0900

    ✨ implement useCurrentUser

--- src/components/parts/layout/TeamPageLayout/TeamPageLayout.tsx
+++ src/components/parts/layout/TeamPageLayout/TeamPageLayout.tsx
@@ -5,10 +5,10 @@ import { Nav, NavItem, NavLink } from 'reactstrap';
 import { useRouter } from 'next/router';
 import { FooterNavbar } from '../FooterNavbar';
 import { Link } from '../../commons';
-import { useCurrentUser } from '~/stores/user/useCurrentUser';
 import { URLS } from '~/constants';
 import { Team } from '~/domains';
 import { TeamIcon } from '~/components/domains/team/TeamIcon';
+import { useCurrentUser } from '~/hooks/CurrentUserProvider/CurrentUserProvider';
 
 const navItems = [
   {
@@ -39,7 +39,7 @@ type Props = {
 export const TeamPageLayout: FC<Props> = ({ team, isMemberOfTeam, children }) => {
   const router = useRouter();
 
-  const { data: currentUser } = useCurrentUser();
+  const { currentUser } = useCurrentUser();
 
   return (
     <div className="min-vh-100 h-100 pb-md-0 pb-5 mb-md-0 mb-5">
--- src/hooks/CurrentUserProvider/CurrentUserProvider.tsx
+++ src/hooks/CurrentUserProvider/CurrentUserProvider.tsx
@@ -1,13 +1,18 @@
-import { createContext, FC, ReactNode, useEffect } from 'react';
+import { createContext, FC, ReactNode, useContext, useEffect } from 'react';
 import useSWR from 'swr';
 import axios from 'axios';
 import { destroyCookie, setCookie } from 'nookies';
 import { useSession } from 'next-auth/react';
-import { User } from '~/domains';
-import { useCurrentUser } from '~/stores/user/useCurrentUser';
+import { convertUserFromServer, User } from '~/domains';
+import { restClient } from '~/utils/rest-client';
+import { Spinner } from '~/components/parts/commons';
+
+export const useCurrentUser = () => {
+  return useContext(CurrentUserContext);
+};
 
 export const CurrentUserContext = createContext<{
-  currentUser?: User | null;
+  currentUser: User | null;
 }>({
   currentUser: null,
 });
@@ -18,7 +23,9 @@ export const CurrentUserProvider: FC<{ children: ReactNode }> = ({ children }) =
   const { data: accessToken, isValidating } = useSWR<string>(status === 'loading' ? null : session?.user?.email, () =>
     axios.get('/api/access-token').then((res) => res.data.accessToken),
   );
-  const { data: currentUser, mutate } = useCurrentUser();
+  const { data: currentUser, mutate } = useSWR('/users/me', (endpoint: string) =>
+    restClient.apiGet<User | null>(endpoint).then((result) => (result.data ? convertUserFromServer(result.data) : null)),
+  );
 
   useEffect(() => {
     if (isValidating) return;
@@ -36,6 +43,13 @@ export const CurrentUserProvider: FC<{ children: ReactNode }> = ({ children }) =
     });
     mutate();
   }, [accessToken, isValidating, mutate]);
+
+  if (currentUser === undefined)
+    return (
+      <div className="mt-5 text-center">
+        <Spinner />
+      </div>
+    );
 
   return <CurrentUserContext.Provider value={{ currentUser }}>{children}</CurrentUserContext.Provider>;
 };
