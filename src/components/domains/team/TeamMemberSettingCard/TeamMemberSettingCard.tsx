6e4fa01bda8dd6b7292a6f5912bcc3ae352a1ee8
kanoi-y <furafuratonbo@gmail.com>
2022-01-13 12:37:24 +0900

    🔨invitationTokenの削除を実装

--- src/components/domains/team/TeamMemberSettingCard/TeamMemberSettingCard.tsx
+++ src/components/domains/team/TeamMemberSettingCard/TeamMemberSettingCard.tsx
@@ -31,6 +31,21 @@ export const TeamMemberSettingCard: VFC<Props> = ({ team }) => {
     }
   };
 
+  const handleCopyInviteLink = (inviteLink: string) => {
+    navigator.clipboard.writeText(inviteLink);
+    notifySuccessMessage('招待リンクをコピーしました!');
+  };
+
+  const handleDeleteInviteLink = async (invitationToken: InvitationToken) => {
+    try {
+      await restClient.apiDelete<InvitationToken>(`/invitation-tokens/${invitationToken._id}`);
+      await mutateInvitationTokens();
+      notifySuccessMessage('招待リンクを削除しました!');
+    } catch (error) {
+      notifyErrorMessage('招待リンクの削除に失敗しました!');
+    }
+  };
+
   return (
     <Card>
       <Button color="primary" onClick={handleCreateInviteLink}>
@@ -39,17 +54,15 @@ export const TeamMemberSettingCard: VFC<Props> = ({ team }) => {
       <div className="mt-4">
         {InvitationTokens.map((invitationToken) => {
           const inviteLink = `${process.env.NEXT_PUBLIC_ROOT_URL}/${team.productId}/invite/${invitationToken.token}`;
-          const handleCopyInviteLink = () => {
-            navigator.clipboard.writeText(inviteLink);
-            notifySuccessMessage('招待リンクをコピーしました!');
-          };
           return (
             <div className="mb-3 d-flex align-items-center gap-2" key={invitationToken._id}>
               <StyledInput className="form-control border-0" readOnly value={inviteLink} />
-              <Button color="primary" outlined onClick={handleCopyInviteLink}>
+              <Button color="primary" outlined onClick={() => handleCopyInviteLink(inviteLink)}>
                 コピー
               </Button>
-              <Button color="danger">削除</Button>
+              <Button color="danger" onClick={() => handleDeleteInviteLink(invitationToken)}>
+                削除
+              </Button>
             </div>
           );
         })}
