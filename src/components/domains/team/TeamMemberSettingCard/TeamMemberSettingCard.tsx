96356e3fd00eed8c73e3dd8f03f2f0e21360edfc
kanoi-y <furafuratonbo@gmail.com>
2022-01-22 14:10:22 +0900

    💄 TeamMemberSettingCardにtableを表示

--- src/components/domains/team/TeamMemberSettingCard/TeamMemberSettingCard.tsx
+++ src/components/domains/team/TeamMemberSettingCard/TeamMemberSettingCard.tsx
@@ -1,7 +1,8 @@
 import React, { useState, VFC } from 'react';
 
+import { UserIcon } from '~/components/domains/user/UserIcon';
 import { ManageInviteLinkModal } from '~/components/domains/team/ManageInviteLinkModal';
-import { Button, Card, Tooltip } from '~/components/parts/commons';
+import { Button, Card, Dropdown, DropdownItem, Icon, Tooltip } from '~/components/parts/commons';
 import { Team, User } from '~/domains';
 
 type Props = {
@@ -12,6 +13,23 @@ type Props = {
 export const TeamMemberSettingCard: VFC<Props> = ({ team, currentUser }) => {
   const [isOpenManageInviteLinkModal, setIsOpenManageInviteLinkModal] = useState(false);
 
+  const handleClickUpdate = () => {};
+
+  const handleClickDelete = () => {};
+
+  const menuItems = [
+    {
+      icon: <Icon icon="CLOCKWISE" size={16} />,
+      text: 'プロダクトの管理者を変更する',
+      onClick: handleClickUpdate,
+    },
+    {
+      icon: <Icon icon="TRASH" size={16} color="DANGER" />,
+      text: 'プロダクトから削除する',
+      onClick: handleClickDelete,
+    },
+  ];
+
   return (
     <>
       <Card>
@@ -20,6 +38,35 @@ export const TeamMemberSettingCard: VFC<Props> = ({ team, currentUser }) => {
             メンバーを招待する
           </Button>
         </Tooltip>
+        <table className="table mt-3">
+          <thead>
+            <tr>
+              <th scope="col">アイコン</th>
+              <th scope="col">ユーザー名</th>
+              <th scope="col">ステータス</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="align-middle">
+              <th scope="row">
+                <UserIcon size={40} />
+              </th>
+              <td>カノイ</td>
+              <td>メンバー</td>
+              <td className="text-end">
+                <Dropdown toggle={<Icon icon="THREE_DOTS_VERTICAL" size={20} />} tag="span">
+                  {menuItems.map((menuItem, i) => (
+                    <DropdownItem key={i} onClick={menuItem.onClick}>
+                      {menuItem.icon}
+                      <span className="ms-2">{menuItem.text}</span>
+                    </DropdownItem>
+                  ))}
+                </Dropdown>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </Card>
       <ManageInviteLinkModal team={team} isOpen={isOpenManageInviteLinkModal} onCloseModal={() => setIsOpenManageInviteLinkModal(false)} />
     </>
