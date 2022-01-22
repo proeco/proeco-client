34e61686f416f69ac89b7539a154b118a83cb7c5
itizawa <itizawa.pen@gmail.com>
2022-01-22 22:45:59 +0900

    💄 adjust style whithout notifications

--- src/components/domains/notification/NotificationBadge/NotificationBadge.tsx
+++ src/components/domains/notification/NotificationBadge/NotificationBadge.tsx
@@ -10,7 +10,9 @@ export const NotificationBadge: VFC<Props> = ({ onClick, count }) => {
   return (
     <button type="button" className="btn btn-sm btn-primary position-relative" onClick={onClick}>
       <Icon icon="BELL" color="WHITE" />
-      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{count > 99 ? '99+' : count}</span>
+      {count > 0 && (
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{count > 99 ? '99+' : count}</span>
+      )}
     </button>
   );
 };
